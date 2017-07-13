f07dd4a0c3b4dff474323c3e9d9fc4b5c3d2efb9
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-07-13 09:56:37 +0800

    [src/scripts] support coverage report

---
+++ src/scripts/walk_through_examples.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -e
+
+if [ "${OUTPUT_DIR}" = "" ] || [ ! -d ${OUTPUT_DIR} ]; then exit 1; fi
+
+PROGS="./mqtt-example"
+cd ${OUTPUT_DIR}/usr/bin
+
+echo ""
+for iter in ${PROGS}; do
+    if [ -f ${iter} ]; then
+        ${iter}
+    fi  
+done
