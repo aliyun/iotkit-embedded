845add42345f45dc609d6ca1a80c5f4161740e66
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-07-18 01:37:33 +0800

    [LITE-utils] support build *.o only necessary

--- src/utils/iot.mk
+++ src/utils/iot.mk
@@ -1,18 +1,18 @@
 LIBA_TARGET := libiot-utils.a
 LIB_SRCS    := $(wildcard */*.c)
 LIB_SRCS    := $(filter-out LITE-utils/lite-utils_prog.c,$(LIB_SRCS))
+LIBHDR_DIR  := lite
+LIB_HEADERS := LITE-utils/{lite-list,lite-utils}.h
 
 DEPENDS     += src/log
-
-LIBHDR_DIR  := lite
 
 PKG_SOURCE  := src/packages/LITE-utils
 # PKG_UPDATE  := 'git@gitlab.alibaba-inc.com:iot-middleware/LITE-utils.git'
 
-build:
-	$(Q)cp utils_config.h LITE-utils/lite-utils_config.h
-	$(Q)$(MAKE) -C LITE-utils \
-	    $(if $(Q),-s,) \
-	    CFLAGS='$(CFLAGS)' \
-	    INS_LIBS=$(LIBA_TARGET) \
-	    library
+PREP_BUILD_HOOK := customize_utils
+define customize_utils
+	$(Q) \
+	if ! diff -q utils_config.h LITE-utils/lite-utils_config.h > /dev/null; then \
+	    cp utils_config.h LITE-utils/lite-utils_config.h; \
+	fi
+endef
