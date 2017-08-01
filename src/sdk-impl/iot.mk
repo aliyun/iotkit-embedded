7e355e66d3131b33fafccdb7274fca2b0f7d9a6a
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-02 00:16:54 +0800

    [build-rules] remove legacy distro logic
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/sdk-impl/iot.mk
+++ src/sdk-impl/iot.mk
@@ -1,7 +1,7 @@
 LIBA_TARGET := libsdk-impl.a
 HDR_REFS    := src
 
-LIBHDR_DIR  := export
+LIBHDR_DIR  := iot-sdk
 LIB_HEADERS := iot_import.h iot_export.h
 LIB_HEADERS += $(wildcard imports/iot_import*.h)
 LIB_HEADERS += $(wildcard exports/iot_export*.h)
