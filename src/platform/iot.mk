8d4090fcce70d216827b1a15e8b0e319926eebb6
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-03 16:38:56 +0800

    [src/platform] contains product/linux/iotx_product_linux.c
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/platform/iot.mk
+++ src/platform/iot.mk
@@ -4,6 +4,7 @@ LIB_SRCS    := $(wildcard *.c)
 LIB_SRCS    += $(wildcard network/$(PLATFORM_NETWORK)/*.c)
 LIB_SRCS    += $(wildcard os/$(PLATFORM_OS)/*.c)
 LIB_SRCS    += $(wildcard ssl/$(PLATFORM_SSL)/*.c)
+LIB_SRCS    += $(wildcard product/$(PLATFORM_OS)/*.c)
 
 DEPENDS     += src/external/recipes/mbedtls
 
