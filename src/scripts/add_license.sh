a990ebe6184147ae79efb85a04ef79c6c0dbe5d8
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-03 22:13:40 +0800

    [scripts] del_license.sh
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/scripts/add_license.sh
+++ src/scripts/add_license.sh
@@ -4,14 +4,14 @@ TEXT=(
     "Copyright (c) 2014-2016 Alibaba Group. All rights reserved."
     "License-Identifier: Apache-2.0"
     ""
-    "Licensed under the Apache License, Version 2.0 (the "License"); you may"
+    "Licensed under the Apache License, Version 2.0 (the \"License\"); you may"
     "not use this file except in compliance with the License."
     "You may obtain a copy of the License at"
     ""
     "http://www.apache.org/licenses/LICENSE-2.0"
     ""
     "Unless required by applicable law or agreed to in writing, software"
-    "distributed under the License is distributed on an "AS IS" BASIS, WITHOUT"
+    "distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT"
     "WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."
     "See the License for the specific language governing permissions and"
     "limitations under the License."
@@ -28,7 +28,11 @@ while (( i >= 0 )); do
     if [ "$j" = "" ]; then
         sed -i "1i\  *" $1
     else
-        sed -i "1i\  * $j" $1
+        if [ "$(echo $j|awk '{ print NF }')" = "1" ] && [ "$(expr substr $j 1 7)" = "http://" ]; then
+            sed -i "1i\  *     $j" $1
+        else
+            sed -i "1i\  * $j" $1
+        fi
     fi
 
     i=$(( i - 1 ))
