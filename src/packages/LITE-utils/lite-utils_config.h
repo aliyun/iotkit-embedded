464cda5ae7d42104d45f285b39cb0dc7f815ccba
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-06 15:05:29 +0800

    [src/guider] move variables definition ahead in function
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/packages/LITE-utils/lite-utils_config.h
+++ src/packages/LITE-utils/lite-utils_config.h
@@ -18,22 +18,6 @@
 #ifndef __LITE_UTILS_CONFIG_H__
 #define __LITE_UTILS_CONFIG_H__
 
-#define LITE_snprintf   snprintf
-#define LITE_printf     printf
-
-#define LITE_log(fmt, args...) \
-    do { \
-        printf(fmt, ##args); \
-        printf("\r\n"); \
-        fflush(stdout); \
-    } while (0)
-
-#define log_crit        LITE_log
-#define log_err         LITE_log
-#define log_info        LITE_log
-#define log_warning     LITE_log
-#define log_debug       LITE_log
-
 #define WITH_MEM_STATS  1
 
 #endif  /* __LITE_UTILS_CONFIG_H__ */
