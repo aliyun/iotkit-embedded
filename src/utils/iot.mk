2d8c655ac99f1168e935437c9381c094ee3833f0
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-07 10:12:21 +0800

    [MAKE] prevent build ID2 when cross-compile
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/utils/iot.mk
+++ src/utils/iot.mk
@@ -5,8 +5,3 @@ LIB_SRCS    := $(filter-out LITE-utils/lite-utils_prog.c,$(LIB_SRCS))
 HDR_REFS    := src
 
 PKG_SOURCE  := src/packages/LITE-utils
-
-PREP_BUILD_HOOK := customize_utils
-define customize_utils
-    $(call Customize_With, utils_config.h, LITE-utils/lite-utils_config.h)
-endef
