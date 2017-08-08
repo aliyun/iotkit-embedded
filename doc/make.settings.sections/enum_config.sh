6cfa1fcc70f172e67e18b6b6655224018d1591c7
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-08 21:19:48 +0800

    [doc] reduce valid configs
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- doc/make.settings.sections/enum_config.sh
+++ doc/make.settings.sections/enum_config.sh
@@ -11,11 +11,12 @@ done
 X=1
 Y=1
 Z=1
-while (( X <= M )); do
+while (( X <= $(echo "${PARA[1]}"|awk '{ print NF }') )); do
     while (( Y <= $(echo "${PARA[2]}"|awk '{ print NF }') )); do
-        while (( Z <= $(echo "${PARA[2]}"|awk '{ print NF }') )); do
+#       while (( Z <= $(echo "${PARA[3]}"|awk '{ print NF }') )); do
 #           echo "$X:$Y:$Z"
-            cat << EOB > make.settings.sample.$X.$Y.$Z
+#           cat << EOB > make.settings.sample.$X.$Y.$Z
+            cat << EOB > make.settings.sample.$X.$Y
 BUILD_TYPE                  = debug
 
 PLATFORM_CC                 = gcc
@@ -25,14 +26,15 @@ FEATURE_MQTT_DEVICE_SHADOW  = y
 EOB
             PART_A=$(echo "${PARA[1]}"|awk "{ print \$${X} }")
             PART_B=$(echo "${PARA[2]}"|awk "{ print \$${Y} }")
-            PART_C=$(echo "${PARA[3]}"|awk "{ print \$${Z} }")
-            CMD="cat ${PART_A} ${PART_B} ${PART_C} >> make.settings.sample.$X.$Y.$Z"
+#           PART_C=$(echo "${PARA[3]}"|awk "{ print \$${Z} }")
+#           CMD="cat ${PART_A} ${PART_B} ${PART_C} >> make.settings.sample.$X.$Y.$Z"
+            CMD="cat ${PART_A} ${PART_B} >> make.settings.sample.$X.$Y"
 #           echo "CMD: ${CMD}"
             eval $CMD
-            Z=$(( Z + 1 ))
-        done
+#           Z=$(( Z + 1 ))
+#       done
         Y=$(( Y + 1 ))
-        Z=1
+#       Z=1
     done    
     X=$(( X + 1 ))
     Y=1
