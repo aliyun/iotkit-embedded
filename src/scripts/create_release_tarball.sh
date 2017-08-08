0f63e5158841b7ff4f1b3529d0a891ccf39bb724
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-08 22:26:09 +0800

    [scripts] hide device info when release
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/scripts/create_release_tarball.sh
+++ src/scripts/create_release_tarball.sh
@@ -2,7 +2,7 @@
 
 VERSION="V2.0"
 DATE_STR=$(date +"%Y%m%d")
-REL_NAME="IoT-SDK_${VERSION}_${DATE_STR}"
+REL_NAME="IoT-SDK_${VERSION}"
 TARBALL="${REL_NAME}.tar.bz2"
 DIFF_STAT="Release_${DATE_STR}.diff"
 
@@ -30,6 +30,11 @@ cd ${WORKDIR}
 sed -i 's:^# PLATFORM_CC.*:# PLATFORM_CC = /path/to/your/cross/gcc:1' make.settings
 sed -i 's:^# PLATFORM_AR.*:# PLATFORM_AR = /path/to/your/cross/ar:1'  make.settings
 find . -name "iot.mk" -exec sed -i '/PKG_UPDATE/d' {} \;
+for iter in $(find . -name "*.[ch]"); do
+    for REPL in PRODUCT_KEY DEVICE_NAME DEVICE_SECRET DEVICE_ID; do
+        sed -i "s!\(#define *[_A-Z]*${REPL} *\)\".*\"!\1\"*******************\"!g" ${iter}
+    done
+done
 
 # Release Section
 cd ${OLDPWD}
