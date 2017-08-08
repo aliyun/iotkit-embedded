e944b25d08834f386a07a2d6827a5382e690c239
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-08 12:48:52 +0800

    [REPO] fix copyright + license has extra space
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/scripts/add_license.sh
+++ src/scripts/add_license.sh
@@ -21,22 +21,22 @@ TOTAL_LINE=${#TEXT[@]}
 i=$(( TOTAL_LINE - 1 ))
 
 sed -i "1i\ " $1
-sed -i "1i\  */" $1
+sed -i "1i\ */" $1
 while (( i >= 0 )); do
     j=$(eval echo '${TEXT['$i']}')
 
     if [ "$j" = "" ]; then
-        sed -i "1i\  *" $1
+        sed -i "1i\ *" $1
     else
         if [ "$(echo $j|awk '{ print NF }')" = "1" ] && [ "$(expr substr $j 1 7)" = "http://" ]; then
-            sed -i "1i\  *     $j" $1
+            sed -i "1i\ *    $j" $1
         else
-            sed -i "1i\  * $j" $1
+            sed -i "1i\ * $j" $1
         fi
     fi
 
     i=$(( i - 1 ))
 done
-sed -i "1i\ /*" $1
+sed -i "1i\/*" $1
 
 sed -i "1,$((TOTAL_LINE+3))s:^ *$::g" $1
