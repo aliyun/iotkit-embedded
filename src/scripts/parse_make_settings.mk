da741def92f0f0e4b32b52802fa02e0d4d678f92
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-09 19:49:38 +0800

    [MAKE] show configs/release-layout when build done
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/scripts/parse_make_settings.mk
+++ src/scripts/parse_make_settings.mk
@@ -7,6 +7,28 @@ $(eval \
 )
 endef
 
+POST_FINAL_OUT_HOOK := Post_Distro
+define Post_Distro
+    @find $(FINAL_DIR) -name "*.[ch]" -exec chmod a-x {} \;
+    @echo ""
+    @echo "============================================================"
+    @echo "o BUILD COMPLETE WITH FOLLOWING SWITCHES:"
+    @echo "----------------------------------------------"
+    @( \
+    $(foreach V,$(SETTING_VARS), \
+        $(if $(findstring FEATURE_,$(V)), \
+            printf "%-32s : %-s\n" "    $(V)" "$($(V))"; \
+        ) \
+    ) )
+    @echo ""
+    @echo "o RELEASE PACKAGE LAYOUT:"
+    @echo "----------------------------------------------"
+    @tree $(FINAL_DIR) --noreport -A -I mbedtls \
+        |awk '{ printf ("    %s\n", $$0); }'
+    @echo "============================================================"
+    @echo ""
+endef
+
 SETTING_VARS := \
     BUILD_TYPE \
     PLATFORM_CC \
