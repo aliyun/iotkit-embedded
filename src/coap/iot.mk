1c5304ba0384d0a077564fc40833195dd6441632
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-08-11 14:29:03 +0800

    [MAKE] enable -Werror @ linuxhost
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/coap/iot.mk
+++ src/coap/iot.mk
@@ -3,8 +3,5 @@ HDR_REFS    := src
 
 DEPENDS     += src/external/mbedtls
 
-# TODO: fix warnings
-CFLAGS := $(filter-out -Werror,$(CFLAGS))
-
 PKG_SOURCE  := src/packages/iot-coap-c
 # PKG_UPDATE  := 'git@gitlab.alibaba-inc.com:iot-middleware/iot-coap-c.git'
