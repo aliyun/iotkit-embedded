2e610da14eaa347460a8f2650d52d9fbfe800058
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-09-08 10:25:12 +0800

    [src/sdk-impl] move iot_export_device.h => iot_export.h
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/sdk-impl/iot_export.h
+++ src/sdk-impl/iot_export.h
@@ -35,6 +35,44 @@ typedef enum _IOT_LogLevel {
     IOT_LOG_DEBUG,
 } IOT_LogLevel;
 
+/* From device.h */
+#define PRODUCT_KEY_LEN     (11)
+#define DEVICE_NAME_LEN     (32)
+#define DEVICE_ID_LEN       (64)
+#define DEVICE_SECRET_LEN   (64)
+
+#define MODULE_VENDOR_ID    (32)    /* Partner ID */
+
+#define HOST_ADDRESS_LEN    (128)
+#define HOST_PORT_LEN       (8)
+#define CLIENT_ID_LEN       (256)
+#define USER_NAME_LEN       (512)   /* Extend length for ID2 */
+#define PASSWORD_LEN        (256)   /* Extend length for ID2 */
+#define AESKEY_STR_LEN      (32)
+#define AESKEY_HEX_LEN      (128/8)
+
+typedef struct {
+    char        product_key[PRODUCT_KEY_LEN + 1];
+    char        device_name[DEVICE_NAME_LEN + 1];
+    char        device_id[DEVICE_ID_LEN + 1];
+    char        device_secret[DEVICE_SECRET_LEN + 1];
+    char        module_vendor_id[MODULE_VENDOR_ID + 1];
+} iotx_device_info_t, *iotx_device_info_pt;
+
+typedef struct {
+    uint16_t        port;
+    char            host_name[HOST_ADDRESS_LEN + 1];
+    char            client_id[CLIENT_ID_LEN + 1];
+    char            username[USER_NAME_LEN + 1];
+    char            password[PASSWORD_LEN + 1];
+    const char     *pub_key;
+#ifdef MQTT_ID2_AUTH
+    char            aeskey_str[AESKEY_STR_LEN];
+    uint8_t         aeskey_hex[AESKEY_HEX_LEN];
+#endif
+} iotx_conn_info_t, *iotx_conn_info_pt;
+/* From device.h */
+
 void    IOT_OpenLog(const char *ident);
 void    IOT_CloseLog(void);
 void    IOT_SetLogLevel(IOT_LogLevel level);
@@ -50,7 +88,6 @@ int     IOT_SetupConnInfoSecure(const char *product_key,
 
 #include "exports/iot_export_errno.h"
 #include "exports/iot_export_mqtt.h"
-#include "exports/iot_export_device.h"
 #include "exports/iot_export_shadow.h"
 #include "exports/iot_export_coap.h"
 #include "exports/iot_export_ota.h"
