dbb66ccffb2bec8a87373c2b81db4dda52d9dbbf
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-10-31 16:26:34 +0800

    [src/tls] fix build failure of missing mbedtls_free()
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/tls/iot.mk
+++ src/tls/iot.mk
@@ -1,4 +1,5 @@
 LIBA_TARGET := libiot-tls.a
 LIB_SRCS    := $(wildcard mbedtls-in-iotkit/library/*.c)
+CFLAGS      := $(filter-out -ansi,$(CFLAGS))
 
 PKG_SOURCE  := src/packages/mbedtls-in-iotkit.git
