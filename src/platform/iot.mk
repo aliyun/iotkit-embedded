72f7e9a7bebc7459660373756fdf32ea67d3e1eb
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-11-08 13:58:22 +0800

    [MAKE] remove PLATFORM_OS|CC|AR switches
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/platform/iot.mk
+++ src/platform/iot.mk
@@ -1,7 +1,7 @@
 LIBA_TARGET := libiot_platform.a
 LIB_SRCS    := $(wildcard *.c)
 
-LIB_SRCS    += $(wildcard os/$(PLATFORM_OS)/*.c)
+LIB_SRCS    += $(wildcard os/$(CONFIG_VENDOR)/*.c)
 LIB_SRCS    += $(wildcard ssl/mbedtls/*.c)
 
 HDR_REFS    += src/sdk-impl
