448afee17935c557cb0b643f57d0eca8e27accbc
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2017-12-26 11:25:17 +0800

    [src/mqtt] modulize with Link-MQTT

--- src/mqtt/iot.mk
+++ src/mqtt/iot.mk
@@ -1,2 +1,6 @@
-LIBA_TARGET := libiot_mqtt.a
-HDR_REFS    := src
+LIBA_TARGET     := libiot_mqtt.a
+HDR_REFS        := src
+DEPENDS         := src/log src/utils
+
+PKG_SOURCE      := Link-MQTT.git
+PKG_UPSTREAM    := git@gitlab.alibaba-inc.com:iot-middleware/Link-MQTT.git
