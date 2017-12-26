5352e648c05b5f140e05c86feb38c18183211222
lifang.ylf <lifang.ylf@alibaba-inc.com>
2017-12-26 16:10:15 +0800

    [src/OTA] ota use git

--- src/ota/iot.mk
+++ src/ota/iot.mk
@@ -1,3 +1,8 @@
-LIBA_TARGET := libiot_ota.a
-HDR_REFS    := src
-LIB_SRCS    := ota.c 
+LIBA_TARGET     := libiot_ota.a
+LIB_SRCS        := $(wildcard Link-OTA/ota.c)
+HDR_REFS        := src
+DEPENDS         := src/log src/utils src/coap src/mqtt
+
+PKG_SOURCE      := Link-OTA.git
+PKG_UPSTREAM    := git@gitlab.alibaba-inc.com:iot-middleware/Link-OTA.git
+
