1bafbeb0dd80ae687c18b8d31b1ec13aaadab38c
lifang.ylf <lifang.ylf@alibaba-inc.com>
2018-02-22 15:52:20 +0800

    [src/cmp modify log-string define]

--- src/cmp/iot.mk
+++ src/cmp/iot.mk
@@ -1,5 +1,7 @@
 LIBA_TARGET     := libiot_cmp.a
 HDR_REFS        := src
 
+CFLAGS          += -DCMP_SUPPORT_TOPIC_DISPATCH \
+
 PKG_SOURCE      := Link-CMP.git
 PKG_UPSTREAM    := git@gitlab.alibaba-inc.com:iot-middleware/Link-CMP.git
