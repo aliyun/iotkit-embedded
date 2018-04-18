a175fb65f45065823007fa2958962642dd831e66
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-04-18 11:22:21 +0800

    [src/scripts] fix permission error
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/scripts/internal_make_funcs.mk
+++ src/scripts/internal_make_funcs.mk
@@ -59,7 +59,7 @@ define Post_Distro
     @if [ "$(CONFIG_LIB_EXPORT)" = "static" ]; then \
         echo "o BINARY FOOTPRINT CONSIST:"; \
         echo "----"; \
-        STAGED=$(LIBOBJ_TMPDIR) STRIP=$(strip $(STRIP)) $(SCRIPT_DIR)/stats_static_lib.sh $(FINAL_DIR)/lib/$(COMP_LIB); \
+        STAGED=$(LIBOBJ_TMPDIR) STRIP=$(strip $(STRIP)) bash $(SCRIPT_DIR)/stats_static_lib.sh $(FINAL_DIR)/lib/$(COMP_LIB); \
     fi
 
     @echo "========================================================================="
