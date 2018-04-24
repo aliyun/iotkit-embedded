aa7f24daaa652018aba59f0599cd9dd13217b469
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-04-24 19:44:00 +0800

    remove unused LIB_HEADERS definition in src/external/tls
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/external/tls/iot.mk
+++ src/external/tls/iot.mk
@@ -1,6 +1,4 @@
 LIBA_TARGET := libiot_tls.a
 CFLAGS      := $(filter-out -ansi,$(CFLAGS))
 
-LIB_HEADERS := $(wildcard $(TOP_DIR)/$(MODULE_NAME)/mbedtls-in-iotkit/include/mbedtls/*.h)
-
 PKG_SOURCE  := mbedtls-in-iotkit.git
