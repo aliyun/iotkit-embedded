ba3c007836d05ccc842d8f98ef9475b02f4bb3bf
ruobin.lb <ruobin.lb@alibaba-inc.com>
2018-04-28 01:45:50 +0800

    rm ota sub git

--- src/framework/services/ota/iot.mk
+++ src/framework/services/ota/iot.mk
@@ -1,6 +1,3 @@
 LIBA_TARGET         := libiot_ota.a
 LIB_SRCS_PATTERN    := Link-OTA/src/ota.c
 HDR_REFS            := src
-
-PKG_SOURCE      := Link-OTA.git
-PKG_UPSTREAM    := git@gitlab.alibaba-inc.com:iot-middleware/Link-OTA.git
