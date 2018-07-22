8771ec7a8da88798c37f13594398411b2855e755
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-07-23 07:12:14 +0800

    [build-rules] adjustment for fix cmake generation
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/services/shadow/iot.mk
+++ src/services/shadow/iot.mk
@@ -1,4 +1,3 @@
 LIBA_TARGET         := libiot_shadow.a
-HDR_REFS            += src/infra/utils
-HDR_REFS            += src/infra/log
-HDR_REFS            += src/services/shadow
+
+HDR_REFS            += src/infra
