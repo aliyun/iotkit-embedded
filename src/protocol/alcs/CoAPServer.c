46476510e33dd92d9cc95ed8080595dc52b1f178
zhangtian <dongren.zt@alibaba-inc.com>
2018-08-16 17:20:58 +0800

    [alcs] increase coap yield task stack size to avoid crash on esp32

--- src/protocol/alcs/CoAPServer.c
+++ src/protocol/alcs/CoAPServer.c
@@ -165,7 +165,7 @@ CoAPContext *CoAPServer_init()
 #ifdef COAP_SERV_MULTITHREAD
         g_coap_running = 1;
         hal_os_thread_param_t task_parms = {0};
-        task_parms.stack_size = 4096;
+        task_parms.stack_size = 4608;
         task_parms.name = "CoAPServer_yield";
         HAL_ThreadCreate(&g_coap_thread, CoAPServer_yield, (void *)g_context, &task_parms, &stack_used);
 #endif
