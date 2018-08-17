2e1aa1d01b32d542bdb56f218e43721352a3efcf
yinghao.cyh <yinghao.cyh@alibaba-inc.com>
2018-08-17 11:13:36 +0800

    [examples] add iot_linkkit_example_countdown implement by new linkkit API

--- examples/iot.mk
+++ examples/iot.mk
@@ -96,9 +96,15 @@ ifneq (,$(filter -DSDK_ENHANCE,$(CFLAGS)))
                                        linkkit/cJSON.c
     endif
 
+    ifeq (,$(filter -DDEPRECATED_LINKKIT,$(CFLAGS)))
     TARGET += linkkit-example-countdown
-    SRCS_linkkit-example-countdown = linkkit/linkkit_example_countdown.c
-
+    SRCS_linkkit-example-countdown = linkkit/iot_linkkit_example_countdown.c
+    endif
+    ifneq (,$(filter -DDEPRECATED_LINKKIT,$(CFLAGS)))
+    TARGET += linkkit-example-countdown
+    SRCS_linkkit-example-countdown = linkkit/linkkit_example_countdown.c 
+    endif
+    
     TARGET += linkkit-example-sched
     SRCS_linkkit-example-sched = linkkit/linkkit_example_sched.c
  
---
+++ examples/linkkit/iot_linkkit_example_countdown.c
@@ -0,0 +1,497 @@
+/*
+ * Copyright (c) 2014-2016 Alibaba Group. All rights reserved.
+ * License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "iot_import.h"
+#include "iot_export.h"
+#include "iot_export_linkkit.h"
+#include "linkkit_export.h"
+#include "cJSON.h"
+
+#define APP_TRACE(fmt, ...)  \
+    do { \
+        HAL_Printf("%s|%03d :: ", __func__, __LINE__); \
+        HAL_Printf(fmt, ##__VA_ARGS__); \
+        HAL_Printf("%s", "\r\n"); \
+    } while(0)
+
+/* json data of property "CountDown", used for debugging in AliCloud console
+{
+  "CountDown": {"IsRunning":1,"TimeLeft":10,"PowerSwitch":1,"Timestamp":"1534314100000"}
+}
+*/
+
+/* Properties used define */
+#define PROPERTY_POWERSWITCH            "PowerSwitch"
+#define PROPERTY_COUNTDOWN              "CountDown"
+#define PROPERTY_ITEM_TIMELEFT          "TimeLeft"
+#define PROPERTY_ITEM_ISRUNNING         "IsRunning"
+#define PROPERTY_ITEM_POWERSWITCH       "PowerSwitch"
+#define PROPERTY_ITEM_TIMESTAMP         "TimeStamp"
+
+/* app context type define */
+typedef struct _app_context {
+    int         devid;
+    int         inited;
+    int         connected;
+
+    void        *timerHandle;
+    char        timestamp[20];
+    int         powerSwitch_Actual;
+    int         powerSwitch_Target;
+} app_context_t;
+
+static app_context_t *app_context;
+
+static void app_set_context(app_context_t *ctx)
+{
+    app_context = ctx;
+}
+
+static void *app_get_context(void)
+{
+    return app_context;
+}
+
+/*
+ * Property CountDown paylaod construction
+ */
+static char *app_construct_countdown_payload(int isrun, int timelf, int pwrsw, char* timestamp)
+{
+    char *payload = NULL;
+    cJSON *root, *prop;
+
+    root = cJSON_CreateObject();
+    if (root == NULL) {
+        return NULL;
+    }
+
+    prop = cJSON_CreateObject();
+    if (root == NULL) {
+        cJSON_Delete(root);
+        return NULL;
+    }
+
+    cJSON_AddItemToObject(root, PROPERTY_COUNTDOWN, prop);
+    cJSON_AddNumberToObject(prop, PROPERTY_ITEM_ISRUNNING, isrun);
+    cJSON_AddNumberToObject(prop, PROPERTY_ITEM_TIMELEFT, timelf);
+    cJSON_AddNumberToObject(prop, PROPERTY_ITEM_POWERSWITCH, pwrsw);
+    cJSON_AddStringToObject(prop, PROPERTY_ITEM_TIMESTAMP, timestamp);
+
+    payload = cJSON_PrintUnformatted(root);
+    cJSON_Delete(root);
+
+    return payload;
+}
+
+/*
+ * Property PowerSwitch paylaod construction
+ */
+static char *app_construct_powerswitch_payload(int pwrsw)
+{
+    char *payload = NULL;
+    cJSON *root = cJSON_CreateObject();
+    if (root == NULL) {
+        return NULL;
+    }
+
+    cJSON_AddNumberToObject(root, PROPERTY_POWERSWITCH, pwrsw);
+
+    payload = cJSON_PrintUnformatted(root);
+    cJSON_Delete(root);
+
+    return payload;
+}
+
+
+/*
+ * Timer expired handle funciton, trigger powerswitch action and post specific property here
+ */
+static void app_timer_expired_handle(void *ctx)
+{
+    int ret = -1;
+    char *payload = NULL;
+
+    app_context_t *app_ctx = app_get_context();
+    if (NULL == app_ctx) {
+        APP_TRACE("can't get app context, just return");
+        return;
+    }
+
+    APP_TRACE("app timer expired!\r\n");
+
+    /* Trigger powerswitch action, just set value here */
+    app_ctx->powerSwitch_Actual = app_ctx->powerSwitch_Target;
+
+    /* Set PowerSwitch property then post */
+    payload = app_construct_powerswitch_payload(app_ctx->powerSwitch_Actual);
+    if (payload == NULL) {
+        return;
+    }
+    ret = IOT_Linkkit_Post(app_ctx->devid, IOTX_LINKKIT_MSG_POST_PROPERTY, NULL, 0, (unsigned char *)payload, strlen(payload));
+    free(payload);
+    if (ret < 0) {
+        APP_TRACE("app post property \"PowerSwitch\" failed");
+        return;        
+    }
+    APP_TRACE("app post property \"PowerSwitch\" succeed, msgID = %d\r\n", ret);
+
+    /* Set CountDown property then post */
+    payload = app_construct_countdown_payload(0, 0, app_ctx->powerSwitch_Target, app_ctx->timestamp);
+    if (payload == NULL) {
+        return;
+    }
+    ret = IOT_Linkkit_Post(app_ctx->devid, IOTX_LINKKIT_MSG_POST_PROPERTY, NULL, 0, (unsigned char *)payload, strlen(payload));
+    free(payload);
+    if (ret < 0) {
+        APP_TRACE("app post property \"CountDown\" failed");
+        return;
+    }
+    APP_TRACE("app post property \"CountDown\" succeed, msgID = %d\r\n", ret);
+}
+
+static void *app_timer_open(void *ctx)
+{
+    return HAL_Timer_Create("App_CountDown", app_timer_expired_handle, ctx);
+}
+
+static void app_timer_close(void *timer)
+{
+    HAL_Timer_Delete(timer);
+}
+
+static void app_timer_stop(void *timer)
+{
+    HAL_Timer_Stop(timer);
+    APP_TRACE("app timer stop");
+}
+
+static void app_timer_start(void *timer, int s)
+{
+    HAL_Timer_Start(timer, s * 1000);
+    APP_TRACE("app timer start");
+}
+
+/* 
+ * cloud connected 
+ */
+static int on_connect(void)
+{
+    app_context_t *app_ctx = app_get_context();
+    if (app_ctx == NULL) {
+        return -1;
+    }
+
+    app_ctx->connected = 1;
+    APP_TRACE("device is connected\n");
+
+    /*
+     * implement user process routine here
+     */
+
+    return 0;
+}
+
+/* 
+ * cloud disconnected
+ */
+static int on_disconnect(void)
+{
+    app_context_t *app_ctx = app_get_context();
+    if (app_ctx == NULL) {
+        return -1;
+    }
+
+    app_ctx->connected = 0;
+    APP_TRACE("device is disconnect\n");
+
+    /*
+     * implement user process routine here
+     */
+
+    return 0;
+}
+
+/*
+ * Property set from cloud handler 
+ */
+static int property_set_handle(const int devid, const char *payload, const int payload_len)
+{
+    int ret = -1;
+    int powerSwitch = 0;
+    int timeLeft = 0;
+    int isRunning = 0;
+    cJSON *root, *prop, *item;
+    app_context_t *app_ctx = app_get_context();
+    if (app_ctx == NULL) {
+        return ret;
+    }
+
+    APP_TRACE("property set, payload: \"%s\"", payload);
+
+    root = cJSON_Parse(payload);
+    if (root == NULL) {
+        APP_TRACE("property set payload is not JSON format");
+        return ret;
+    }
+
+    prop = cJSON_GetObjectItem(root, PROPERTY_COUNTDOWN);
+    if (prop == NULL || !cJSON_IsObject(prop)) {
+        prop = cJSON_GetObjectItem(root, PROPERTY_POWERSWITCH);
+        if (prop == NULL || !cJSON_IsNumber(prop)) {
+            cJSON_Delete(root);
+            return ret;
+        }
+        APP_TRACE("property is PowerSwitch");
+        app_ctx->powerSwitch_Actual = prop->valueint;
+        APP_TRACE("PowerSwitch actual value set to %d\r\n", app_ctx->powerSwitch_Actual);
+
+        cJSON_Delete(root);
+        return ret;  
+    }
+
+    APP_TRACE("property is CountDown");
+
+    /* Get powerSwitch value */
+    item = cJSON_GetObjectItem(prop, PROPERTY_ITEM_POWERSWITCH);
+    if (item != NULL && cJSON_IsNumber(item)) {
+        powerSwitch = item->valueint;
+        APP_TRACE("PowerSwitch target value is %d", powerSwitch);
+    } else {
+        cJSON_Delete(root);
+        return ret;
+    }
+
+    /* Get timeLeft value */
+    item = cJSON_GetObjectItem(prop, PROPERTY_ITEM_TIMELEFT);
+    if (item != NULL && cJSON_IsNumber(item)) {
+        timeLeft = item->valueint;
+        APP_TRACE("TimeLeft is %d", timeLeft);
+    } else {
+        cJSON_Delete(root);
+        return ret;
+    }
+
+    /* Get isRunning value */
+    item = cJSON_GetObjectItem(prop, PROPERTY_ITEM_ISRUNNING);
+    if (item != NULL && cJSON_IsNumber(item)) {
+        isRunning = item->valueint;
+        APP_TRACE("IsRunning is %d", isRunning);
+    } else {
+        cJSON_Delete(root);
+        return ret;
+    }
+
+    /* Get timeStamp value */
+    item = cJSON_GetObjectItem(prop, PROPERTY_ITEM_TIMESTAMP);
+    if (item != NULL && cJSON_IsString(item)) {
+        strcpy(app_ctx->timestamp, item->valuestring);
+        APP_TRACE("Timestamp is %s", app_ctx->timestamp);
+    } else {
+        cJSON_Delete(root);
+        return ret;
+    }
+
+    /* Delete Json */
+    cJSON_Delete(root);
+
+    /* Start or stop timer according to "IsRunning" */
+    if (isRunning == 1) {
+        app_timer_start(app_ctx->timerHandle, timeLeft);
+        /* temp powerswitch value to app context */
+        app_ctx->powerSwitch_Target = powerSwitch;
+    } else if (isRunning == 0) {
+        app_timer_stop(app_ctx->timerHandle);
+    }
+
+    /* Just echo the CountDown property */
+    ret = IOT_Linkkit_Post(app_ctx->devid, IOTX_LINKKIT_MSG_POST_PROPERTY, NULL, 0, (unsigned char *)payload, payload_len);
+    if (ret < 0) {
+        APP_TRACE("app post property \"CountDown\" failed");
+        return ret; 
+    }
+    APP_TRACE("app post property \"CountDown\" succeed, msgID = %d\r\n", ret);
+
+    /* Post the PowerSwitch property, powerSwitch_Actual used */
+    char *payload_pwrsw = app_construct_powerswitch_payload(app_ctx->powerSwitch_Actual);
+
+    ret = IOT_Linkkit_Post(app_ctx->devid, IOTX_LINKKIT_MSG_POST_PROPERTY, NULL, 0, (unsigned char *)payload_pwrsw, strlen(payload_pwrsw));
+    free(payload_pwrsw);
+    if (ret < 0) {
+        APP_TRACE("app post property \"PowerSwitch\" failed");
+        return ret; 
+    }
+    APP_TRACE("app post property \"PowerSwitch\" secceed, msgID = %d\r\n", ret);
+
+    return 0;
+}
+
+static int post_reply_handle(const int devid, const int msgid, const int code, const char *payload, const int payload_len)
+{
+    APP_TRACE("thing@%p: response arrived: {id:%d, code:%d, message:%s}\n", devid, msgid, code,
+              payload == NULL ? "NULL" : payload);
+
+    /*
+     * implement user process routine here
+     */
+
+    return 0;
+}
+
+/*
+ * Device inited handle 
+ */
+static int initialized_handle(const int devid)
+{
+    app_context_t *app_ctx = app_get_context();
+
+    app_ctx->inited = 1;
+    APP_TRACE("device %d inited\n", devid);
+
+    /*
+     * implement user process routine here
+     */
+
+    return 0;
+}
+
+static uint64_t user_update_sec(void)
+{
+    uint64_t start_time = 0;
+
+    if (start_time == 0) {
+        start_time = HAL_UptimeMs();
+    }
+
+    return (HAL_UptimeMs() - start_time) / 1000;
+}
+
+/*
+ * linkkit timer countdown example routine 
+ */
+int linkkit_example()
+{
+    int ret = 0;
+    int property_opt = 1;
+    uint64_t time_prev_sec = 0, time_now_sec = 0;
+    app_context_t app_ctx = {0};
+
+    /* init app context */
+    app_ctx.timerHandle = app_timer_open(NULL);
+    app_ctx.powerSwitch_Actual = 0;     /* assume initial status of PowerSwitch is 0 */
+    app_ctx.powerSwitch_Target = 0;
+    app_set_context(&app_ctx);
+
+    /* init linkkit device metadata */
+    iotx_linkkit_dev_meta_info_t linkkit_meta_info = {
+        .product_key    = "a1ikrQdGiG8",
+        .product_secret = "UgqWfzL5B2zNvglF",
+        .device_name    = "CountDown1",
+        .device_secret  = "kjr4EItbk13LIRYlvtYSIR6UTSXwWzif"
+    };
+
+    /* init linkkit event handle function */
+    iotx_linkkit_event_handler_t linkkit_event_handler = {
+        .connected              = on_connect,
+        .disconnected           = on_disconnect,
+        .down_raw               = NULL,
+        .up_raw_reply           = NULL,
+        .async_service_request  = NULL,
+        .sync_service_request   = NULL,
+        .property_set           = property_set_handle,
+        .post_reply             = post_reply_handle,
+        .ntp_response           = NULL,
+        .permit_join            = NULL,
+        .initialized            = initialized_handle,
+    };
+
+    /*
+     * Open linkkit device
+     */
+    app_ctx.devid = IOT_Linkkit_Open(IOTX_LINKKIT_DEV_TYPE_MASTER, &linkkit_meta_info);
+    if (ret < 0) {
+        APP_TRACE("linkkit device open fail");
+        return -1;
+    }
+    
+    /*
+     * Config linkkit device option 
+     */
+    IOT_Linkkit_Ioctl(app_ctx.devid, IOTX_LINKKIT_CMD_OPTION_PROPERTY_POST_REPLY, &property_opt);
+
+    /*
+     * Start device network connection
+     */ 
+    ret = IOT_Linkkit_Start(app_ctx.devid, &linkkit_event_handler);
+    if (ret < 0) {
+        APP_TRACE("linkkit start fail");
+        return -1;  
+    }
+
+    APP_TRACE("linkkit started, enter loop");
+    while (1) {
+        /* Do linkkit yeild first */
+        IOT_Linkkit_Yield(200);
+        
+        time_now_sec = user_update_sec();
+        if (time_prev_sec == time_now_sec) {
+            continue;
+        }
+
+
+        if (time_now_sec % 10) {
+            /*
+            * implement user process routine here
+            */
+        }
+
+        time_prev_sec = time_now_sec;
+    }
+
+    /* app end */
+    IOT_Linkkit_Close(app_ctx.devid);
+    app_timer_stop(app_ctx.timerHandle);
+    app_timer_close(app_ctx.timerHandle);
+
+    return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+    IOT_OpenLog("linkkit");
+    IOT_SetLogLevel(IOT_LOG_INFO);
+
+    APP_TRACE("start!\n");
+    /*
+     * linkkit timer countdown dome
+     * please check document: https://help.aliyun.com/document_detail/73708.html?spm=a2c4g.11174283.6.560.zfcQ3y
+     *         API introduce:  https://help.aliyun.com/document_detail/68687.html?spm=a2c4g.11186623.6.627.RJcT3F
+     */
+    linkkit_example();
+
+    IOT_DumpMemoryStats(IOT_LOG_DEBUG);
+    IOT_CloseLog();
+
+    APP_TRACE("out of example!\n");
+
+    return 0;
+}
