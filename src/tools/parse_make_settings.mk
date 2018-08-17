cfa74326e04428ed397eacb864dc49b87eaca237
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-08-17 15:08:37 +0800

    [tools] search default_settings for FEATURE_XXX as well
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/tools/parse_make_settings.mk
+++ src/tools/parse_make_settings.mk
@@ -17,8 +17,10 @@ SWITCH_VARS :=  \
     FEATURE_SUBDEVICE_ENABLED \
     FEATURE_SDK_ENHANCE \
 
-SWITCH_VARS += $(shell grep -o 'FEATURE_[_A-Z0-9]*' $(TOP_DIR)/make.settings|uniq)
+SWITCH_VARS += $(shell grep -o 'FEATURE_[_A-Z0-9]*' $(TOP_DIR)/make.settings $(TOP_DIR)/src/tools/default_settings.mk|cut -d: -f2|uniq)
 SWITCH_VARS := $(sort $(SWITCH_VARS))
+
+$(warning $(SWITCH_VARS))
 
 $(foreach v, \
     $(SWITCH_VARS), \
