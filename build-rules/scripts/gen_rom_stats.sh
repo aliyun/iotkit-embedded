c976effb836d927062304f9f6e951668415c55e9
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-09-20 13:12:27 +0800

    [build-rules] update for ROM footprint statistics
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- build-rules/scripts/gen_rom_stats.sh
+++ build-rules/scripts/gen_rom_stats.sh
@@ -4,32 +4,32 @@
 #echo "STRIP = ${STRIP}"
 
 echo ""
-printf "    | %-8s | %-32s | %12s | %24s\n" "RATE" "OBJ NAME" "BYTES/TOTAL" "MODULE NAME"
+printf "    | %-5s | %-35s | %14s | %26s |\n" "RATE" "OBJ NAME" "BYTES/TOTAL" "MODULE NAME"
 
 for iter in ${ALL_SUB_DIRS}; do
     cd ${OUTPUT_DIR}/${iter}
 
     ITER_OBJS=$(find . -name "*.o")
     [ "${ITER_OBJS}" != "" ] || continue
-    ${STRIP} ${ITER_OBJS}
+    ${STRIP} ${ITER_OBJS} 2>/dev/null
 
-    printf "    |-%-.8s-|-%-.32s-|-%.12s-|-%.24s-|\n" \
+    printf "    |-%-.5s-|-%-.35s-|-%.14s-|-%.26s-|\n" \
         "-----------------------------------------" \
         "-----------------------------------------" \
         "-----------------------------------------" \
         "-----------------------------------------"
 
-    ITER_SIZE=$(size ${ITER_OBJS} | sed '1d' | awk '{ sum += $1 } END { print sum }')
+    ITER_SIZE=$(size ${ITER_OBJS} | ${SED} '1d' | awk '{ sum += $1 } END { print sum }')
     for j in ${ITER_OBJS}; do
-        size $j | sed '1d' \
+        size $j | ${SED} '1d' \
             | awk -v sum="${ITER_SIZE}" \
                   -v mod="${iter}" \
                   -v obj="$(basename ${j})" \
-                  '{ printf("%d %.2f%% %s %-d/%d %s\n", $1, ($1/sum)*100, obj, $1, sum, mod); }'
+                  '{ if ($1 != 0) printf("%d %.4s%% %s %-d/%d %s\n", $1, ($1/sum)*100, obj, $1, sum, mod); }'
     done \
          | sort -nr \
          | cut -d' ' -f2- \
-         | awk '{ printf("    | %-8s | %-32s | %12s | %24s |\n", $1, $2, $3, $4); }'
+         | awk '{ printf("    | %-5s | %-35s | %14s | %26s |\n", $1, $2, $3, $4); }'
 
     cd ${OLDPWD}
 done
@@ -38,24 +38,31 @@ echo ""
 echo ""
 echo ""
 
-printf "    | %-43s | %-8s | %-8s | %-8s | %-4s |\n" \
-    "MODULE NAME" "ROM" "RAM" "BSS" "DATA"
-printf "    |-%-.43s-|-%-.8s-|-%-.8s-|-%-.8s-|-%-.4s-|\n" \
+printf "    | %-5s | %-35s | %-9s | %-9s | %-10s | %-6s |\n" \
+    "RATE" "MODULE NAME" "ROM" "RAM" "BSS" "DATA"
+printf "    |-%-.5s-|-%-.35s-|-%-.9s-|-%-.9s-|-%-.10s-|-%-.6s-|\n" \
+    "-------------" \
     "--------------------------------------------" \
     "-------------" \
     "-------------" \
     "-------------" \
     "-------------"
 
+cd ${OUTPUT_DIR}
+TOTAL_ROM=$(find . -name "*.o" -not -path "*$(basename ${LIBOBJ_TMPDIR})*" \
+    | xargs size \
+    | awk '{ sum += $1 } END { print sum }')
+cd ${OLDPWD}
+
 for iter in ${ALL_SUB_DIRS}; do
     cd ${OUTPUT_DIR}/${iter}
 
     ITER_OBJS=$(find . -name "*.o")
     [ "${ITER_OBJS}" != "" ] || continue
-    ${STRIP} ${ITER_OBJS}
+    ${STRIP} ${ITER_OBJS} 2>/dev/null
 
-    size ${ITER_OBJS} | sed '1d' \
-        | awk -v name=${iter} '
+    size ${ITER_OBJS} | ${SED} '1d' \
+        | awk -v name=${iter} -v total_rom=${TOTAL_ROM} '
             BEGIN { sum_rom = sum_ram = sum_bss = sum_data = 0 }
             {
                 sum_rom += $1 + $2;
@@ -63,8 +70,11 @@ for iter in ${ALL_SUB_DIRS}; do
                 sum_bss += $3;
                 sum_data += $2;
             }
-            END { printf("%d | %-43s | %-8d | %-8d | %-8d | %-4d |\n",
+            END {
+                rate = sum_rom / total_rom * 100;
+                printf("%d | %.4s%% | %-35s | %-9s | %-9s | %-10s | %-6s |\n",
                     sum_rom,
+                    rate,
                     name,
                     sum_rom,
                     sum_ram,
@@ -77,19 +87,20 @@ for iter in ${ALL_SUB_DIRS}; do
 done \
     | sort -nr \
     | cut -d' ' -f2- \
-    | sed 's!.*!    &!g' \
-    | awk '
+    | ${SED} 's!.*!    &!g' \
+    | awk -v total_rom=${TOTAL_ROM} '
         BEGIN { sum_rom = sum_ram = sum_bss = sum_data = 0 }
         {
-            sum_rom += $4;
-            sum_ram += $6;
-            sum_bss += $8;
-            sum_data += $10;
+            sum_rom += $6;
+            sum_ram += $8;
+            sum_bss += $10;
+            sum_data += $12;
             print;
         }
         END {
-            printf("    |---------------------------------------------|----------|----------|----------|------|\n");
-            printf("    | %-43s | %-8d | %-8d | %-8d | %-4d |\n", "- IN TOTAL -", sum_rom, sum_ram, sum_bss, sum_data);
+            rate = sum_rom / total_rom * 100;
+            printf("    |-------|-------------------------------------|-----------|-----------|------------|--------|\n");
+            printf("    |  %.3s%% | %-35s | %-9s | %-9s | %-10s | %-6s |\n", rate, "- IN TOTAL -", sum_rom, sum_ram, sum_bss, sum_data);
         }
         '
 
