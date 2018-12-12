d0c44ddcd9f7ea9da5ab05f07d2085afea87e688
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-12-13 00:30:26 +0800

    [src/infra] add FEATURE_PLATFORM_HAS_STDINT to control infra_types.h
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- src/infra/infra_types.h
+++ src/infra/infra_types.h
@@ -1,3 +1,5 @@
+#if !defined(PLATFORM_HAS_STDINT)
+
 #ifndef _INFRA_TYPES_H_
 #define _INFRA_TYPES_H_
 
@@ -5,13 +7,18 @@
 // #define NULL ((void*)0)
 // #endif
 
-typedef unsigned char     uint8_t;
-typedef signed char       int8_t;
-typedef unsigned short    uint16_t;
-typedef signed short      int16_t;
-typedef unsigned int      uint32_t;
-typedef signed int        int32_t;
-typedef unsigned long int uint64_t;
-typedef signed long int   int64_t;
+typedef unsigned char       uint8_t;
+typedef signed char         int8_t;
+typedef unsigned short      uint16_t;
+typedef signed short        int16_t;
+typedef unsigned int        uint32_t;
+typedef signed int          int32_t;
+typedef unsigned long int   uint64_t;
+typedef signed long int     int64_t;
 
 #endif
+#else
+
+#include <stdint.h>
+
+#endif  /* #if !defined(PLATFORM_HAS_STDINT) */
