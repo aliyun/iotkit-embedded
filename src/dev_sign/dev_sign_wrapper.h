b7252a3a0240237a22ec28b70c437678a66bbff0
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2018-12-17 16:36:06 +0800

    [dev sign] rename HAL_Snprintf declaration

--- src/dev_sign/dev_sign_wrapper.h
+++ src/dev_sign/dev_sign_wrapper.h
@@ -5,4 +5,4 @@
 
 void *HAL_Malloc(uint32_t size);
 void HAL_Free(void *ptr);
-int8_t HAL_Snprintf(char *str, const int len, const char *fmt, ...);
+int HAL_Snprintf(char *str, const int len, const char *fmt, ...);
