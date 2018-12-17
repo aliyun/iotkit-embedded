9c84ea170e864ebd761da76011af9d37b202352a
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2018-12-17 16:35:04 +0800

    [dev_sign] remove unused wrapper

--- src/dev_sign/dev_sign_wrapper.h
+++ src/dev_sign/dev_sign_wrapper.h
@@ -3,13 +3,6 @@
 #include "infra_types.h"
 #include "infra_defs.h"
 
-int8_t HAL_GetPartnerID(char *pid_str);
-int8_t HAL_GetModuleID(char *mid_str);
-int8_t HAL_GetProductKey(char product_key[IOTX_PRODUCT_KEY_LEN]);
-int8_t HAL_GetDeviceName(char device_name[IOTX_DEVICE_NAME_LEN]);
-int8_t HAL_GetDeviceSecret(char device_secret[IOTX_DEVICE_SECRET_LEN]);
-
 void *HAL_Malloc(uint32_t size);
 void HAL_Free(void *ptr);
 int8_t HAL_Snprintf(char *str, const int len, const char *fmt, ...);
-uint64_t HAL_UptimeMs(void);
