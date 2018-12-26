e151fcc016436c7602b4776f526c062c28f149b4
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2018-12-26 21:03:25 +0800

    update config.bat

--- config.bat
+++ config.bat
@@ -1,14 +1,11 @@
 @echo off
-chcp 65001
+chcp 437
 
 set CONFIG_=FEATURE_
 
 .\tools\prebuilt\windows\kconfig-frontends-3.12.0-windows\kconfig-mconf.exe .\tools\Config.in
 
 if exist .config (
-	echo .config is exist
 	copy /y .config make.settings
 	del /F .config
 )
-
-pause
