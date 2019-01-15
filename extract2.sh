030443ed43bb2872f5e0233215b906db4c40bb1d
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-01-15 10:41:22 +0800

    [extract2.sh] finish HAL/Wrapper function extract

--- extract2.sh
+++ extract2.sh
@@ -2,6 +2,7 @@
 
 OUTPUT_DIR=output
 INFRA_DIR=${OUTPUT_DIR}/eng/infra
+WRAPPERS_DIR=${OUTPUT_DIR}/eng/wrappers
 
 XTRC_FILE_RULS=./tools/misc/xtrc_file_rules
 XTRC_WRAPPER_RULS=./tools/misc/xtrc_wrapper_rules
@@ -52,7 +53,6 @@ cond_not_check()
 # Read xtrc_file_rules
 cat ${XTRC_FILE_RULS} | while read rule
 do
-    echo $rule
     COND_AND=$(echo $rule | awk -F'|' '{print $1}')
     COND_NOT=$(echo $rule | awk -F'|' '{print $2}')
     SRC_DIR=$(echo $rule | awk -F'|' '{print $3}')
@@ -70,4 +70,69 @@ do
     # echo "${DEST_DIR}"
 
     [[ ${DEST_DIR} ]] && mkdir -p ${DEST_DIR} && find ${SRC_DIR} -maxdepth 1 -name *.[ch] | xargs -i cp -rf {} ${DEST_DIR}
+done
+
+# Generate wrapper.c
+mkdir -p ${WRAPPERS_DIR}
+cp -f wrappers/wrappers_defs.h ${WRAPPERS_DIR}/
+
+# Read xtrc_wrapper_rules
+while read rule
+do
+    # echo $rule
+    COND_AND=$(echo $rule | awk -F'|' '{print $1}')
+    COND_NOT=$(echo $rule | awk -F'|' '{print $2}')
+    FUNC_NAME=$(echo $rule | awk -F'|' '{print $3}')
+    HEADER_FILE=$(echo $rule | awk -F'|' '{print $4}')
+
+    # echo "${COND_AND}"
+    cond_and_check "${COND_AND}"
+    [[ $? -ne 0 ]] && continue
+
+    # echo "${COND_NOT}"
+    cond_not_check "${COND_NOT}"
+    [[ $? -eq 0 ]] && continue
+
+    [[ ${FUNC_NAME} ]] && FUNC_NAME_LIST+=${FUNC_NAME}"\n"
+    [[ ${HEADER_FILE} ]] && HEADER_FILE_LIST+=${HEADER_FILE}"\n"
+    
+    # [[ ${DEST_DIR} ]] && mkdir -p ${DEST_DIR} && find ${SRC_DIR} -maxdepth 1 -name *.[ch] | xargs -i cp -rf {} ${DEST_DIR}
+done < ${XTRC_WRAPPER_RULS}
+
+FUNC_NAME_LIST=$(echo -e "${FUNC_NAME_LIST}" | sed -n '/^$/!{p}' | sort -u)
+HEADER_FILE_LIST=$(echo -e "${HEADER_FILE_LIST}" | sed -n '/^$/!{p}' | sort -u)
+
+# For Debug
+echo -e "\nHAL/Wrapper Function List:"
+echo -e "${FUNC_NAME_LIST}" |awk '{ printf("%03d %s\n", NR, $0); }'
+echo -e "\nHAL/Wrapper Header File List:"
+echo -e "${HEADER_FILE_LIST}" |awk '{ printf("%03d %s\n", NR, $0); }'
+echo ""
+
+# Annotation For wrapper.c
+sed -n  '/WRAPPER_NOTE:/{:a;N;/*\//!ba;p}' ${WRAPPER_DOC} | sed -n '1d;p' >> ${WRAPPERS_DIR}/wrapper.c
+
+# Output Header File Into wrapper.c
+echo -e "#include \"infra_types.h\"" >> ${WRAPPERS_DIR}/wrapper.c
+echo -e "#include \"infra_defs.h\"" >> ${WRAPPERS_DIR}/wrapper.c
+echo -e "#include \"wrappers_defs.h\"" >> ${WRAPPERS_DIR}/wrapper.c
+echo -e "${HEADER_FILE_LIST}" | sed -n '/.h/{s/^/#include "/p}' | sed -n 's/$/"/p' >> ${WRAPPERS_DIR}/wrapper.c
+echo -e "" >> ${WRAPPERS_DIR}/wrapper.c
+
+# Generate Default Implenmentation For HAL/Wrapper Function
+echo -e "${FUNC_NAME_LIST}" | while read func
+do
+    [[ ! ${func} ]] && return
+
+    FUNC_DEC=$(find ./${OUTPUT_DIR}/eng -name *wrapper.h | xargs -i cat {} 2>/dev/null | sed -n '/.*'$func'(.*/{/.*);/ba;{:c;N;/.*);/!bc};:a;p;q}')
+    DATA_TYPE=$(echo "${FUNC_DEC}" | head -1 | awk -F'wrapper|HAL' '{print $1}' | sed s/[[:space:]]//g)
+    # echo "${func}"
+
+    sed -n '/'${func}':/{:a;N;/*\//!ba;p}' ${WRAPPER_DOC} | sed -n '1d;p' >> ${WRAPPERS_DIR}/wrapper.c
+
+    if [ "${DATA_TYPE}" == "void" ];then
+        echo "${FUNC_DEC}" | sed -n '/;/{s/;/\n{\n\treturn;\n}\n\n/g};p' >> ${WRAPPERS_DIR}/wrapper.c
+    else
+        echo "${FUNC_DEC}" | sed -n '/;/{s/;/\n{\n\treturn ('${DATA_TYPE}')1;\n}\n\n/g};p' >> ${WRAPPERS_DIR}/wrapper.c
+    fi
 done
