15ff9421f40823207bbf1bce7631d8f5562f3194
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-01-16 15:46:19 +0800

    "[extract.sh] support win-bash, fix problem caused by IFS"
    This reverts commit 1366049facc03df17edcd63c7139be89dcc380e5.

--- extract.sh
+++ extract.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+OLD_IFS=$IFS
+IFS=$'\n'
+
 OUTPUT_DIR=output
 INFRA_DIR=${OUTPUT_DIR}/eng/infra
 WRAPPERS_DIR=${OUTPUT_DIR}/eng/wrappers
@@ -20,9 +23,9 @@ echo -e "#define _INFRA_CONFIG_H_\n" >> ${INFRA_DIR}/infra_config.h
 echo "${MACRO_LIST}" | sed -n 's/^/#define /p' >> ${INFRA_DIR}/infra_config.h
 echo -e "\n#endif" >> ${INFRA_DIR}/infra_config.h
 
-find . -path ./${OUTPUT_DIR} -prune -type f -o -name "infra_types.h" | xargs -i cp -f {} ${INFRA_DIR}
-find . -path ./${OUTPUT_DIR} -prune -type f -o -name "infra_defs.[ch]" | xargs -i cp -f {} ${INFRA_DIR}
-find . -path ./${OUTPUT_DIR} -prune -type f -o -name "infra_compat.h" | xargs -i cp -f {} ${INFRA_DIR}
+find ./src -name "infra_types.h" | xargs -i cp -f {} ${INFRA_DIR}
+find ./src -name "infra_defs.[ch]" | xargs -i cp -f {} ${INFRA_DIR}
+find ./src -name "infra_compat.h" | xargs -i cp -f {} ${INFRA_DIR}
 
 # echo -e "${MACRO_LIST}"
 
@@ -33,7 +36,9 @@ cond_and_check()
     for item in ${COND_AND_VAR}
     do
         echo ${MACRO_LIST} | grep -wo ${item} > /dev/null
-        [[ $? -ne 0 ]] && return -1
+        if [ $? -ne 0 ];then
+            return -1
+        fi
     done
     return 0
 }
@@ -45,16 +50,20 @@ cond_not_check()
     for item in ${COND_NOT_VAR}
     do
         echo ${MACRO_LIST} | grep -wo ${item} > /dev/null
-        [[ $? -eq 0 ]] && return 0
+        if [ $? -eq 0 ];then
+            return 0
+        fi
     done
     return -1
 }
 
 # Read xtrc_file_rules
-cat ${XTRC_FILE_RULS} | while read rule
+for rule in $(cat ${XTRC_FILE_RULS})
 do
     TEST=$(echo $rule | awk -F'|' '{print NF}')
-    [[ ${TEST} -ne 4 ]] && continue
+    if [ ${TEST} -ne 4 ];then
+        continue
+    fi
 
     COND_AND=$(echo $rule | awk -F'|' '{print $1}')
     COND_NOT=$(echo $rule | awk -F'|' '{print $2}')
@@ -63,16 +72,24 @@ do
 
     # echo "${COND_AND}"
     cond_and_check "${COND_AND}"
-    [[ $? -ne 0 ]] && continue
+    if [ $? -ne 0 ];then
+        continue
+    fi
 
     # echo "${COND_NOT}"
     cond_not_check "${COND_NOT}"
-    [[ $? -eq 0 ]] && continue
+    if [ $? -eq 0 ];then
+        continue
+    fi
 
     # echo "${SRC_DIR}"
     # echo "${DEST_DIR}"
 
-    [[ ${DEST_DIR} ]] && mkdir -p ${DEST_DIR} && find ${SRC_DIR} -maxdepth 1 -name *.[ch] | xargs -i cp -rf {} ${DEST_DIR}
+    IFS=$OLD_IFS
+    if [ "${DEST_DIR}" != "" ];then
+        mkdir -p ${DEST_DIR} && find ${SRC_DIR} -maxdepth 1 -name *.[ch] | xargs -i cp -rf {} ${DEST_DIR}
+    fi
+    IFS=$'\n'
 done
 
 # Generate wrapper.c
@@ -80,10 +97,12 @@ mkdir -p ${WRAPPERS_DIR}
 cp -f wrappers/wrappers_defs.h ${WRAPPERS_DIR}/
 
 # Read xtrc_wrapper_rules
-while read rule
+for rule in $(cat ${XTRC_WRAPPER_RULS})
 do
     TEST=$(echo $rule | awk -F'|' '{print NF}')
-    [[ ${TEST} -ne 4 ]] && continue
+    if [ ${TEST} -ne 4 ];then
+        continue
+    fi
 
     COND_AND=$(echo $rule | awk -F'|' '{print $1}')
     COND_NOT=$(echo $rule | awk -F'|' '{print $2}')
@@ -92,24 +111,35 @@ do
 
     # echo "${COND_AND}"
     cond_and_check "${COND_AND}"
-    [[ $? -ne 0 ]] && continue
+    if [ $? -ne 0 ];then
+        continue
+    fi
 
     # echo "${COND_NOT}"
     cond_not_check "${COND_NOT}"
-    [[ $? -eq 0 ]] && continue
+    if [ $? -eq 0 ];then
+        continue
+    fi
 
-    [[ ${FUNC_NAME} ]] && FUNC_NAME_LIST+=${FUNC_NAME}"\n"
-    [[ ${HEADER_FILE} ]] && HEADER_FILE_LIST+=${HEADER_FILE}"\n"
-    
-    # [[ ${DEST_DIR} ]] && mkdir -p ${DEST_DIR} && find ${SRC_DIR} -maxdepth 1 -name *.[ch] | xargs -i cp -rf {} ${DEST_DIR}
-done < ${XTRC_WRAPPER_RULS}
+    if [ "${FUNC_NAME}" != "" ];then
+        FUNC_NAME_LIST="${FUNC_NAME_LIST}""${FUNC_NAME}\n"
+    fi
+    if [ "${HEADER_FILE}" != "" ];then
+        HEADER_FILE_LIST="${HEADER_FILE_LIST}""${HEADER_FILE}\n"
+    fi
+done
 
 FUNC_NAME_LIST=$(echo -e "${FUNC_NAME_LIST}" | sed -n '/^$/!{p}' | sort -u)
 HEADER_FILE_LIST=$(echo -e "${HEADER_FILE_LIST}" | sed -n '/^$/!{p}' | sort -u)
 
 # For Debug
-[[ ${FUNC_NAME_LIST} ]] && echo -e "\nHAL/Wrapper Function List:" && echo -e "${FUNC_NAME_LIST}" |awk '{ printf("%03d %s\n", NR, $0); }'
-[[ ${HEADER_FILE_LIST} ]] && echo -e "\nHAL/Wrapper Header File List:" && echo -e "${HEADER_FILE_LIST}" |awk '{ printf("%03d %s\n", NR, $0); }' && echo ""
+if [ "${FUNC_NAME_LIST}" != "" ];then
+    echo -e "\nHAL/Wrapper Function List:" && echo -e "${FUNC_NAME_LIST}" |awk '{ printf("%03d %s\n", NR, $0); }'
+fi
+
+if [ "${HEADER_FILE_LIST}" != "" ];then
+    echo -e "\nHAL/Wrapper Header File List:" && echo -e "${HEADER_FILE_LIST}" |awk '{ printf("%03d %s\n", NR, $0); }' && echo ""
+fi
 
 # Annotation For wrapper.c
 sed -n  '/WRAPPER_NOTE:/{:a;N;/*\//!ba;p}' ${WRAPPER_DOC} | sed -n '1d;p' >> ${WRAPPERS_DIR}/wrapper.c
@@ -122,17 +152,22 @@ echo -e "${HEADER_FILE_LIST}" | sed -n '/.h/{s/^/#include "/p}' | sed -n 's/$/"/
 echo -e "" >> ${WRAPPERS_DIR}/wrapper.c
 
 # Generate Default Implenmentation For HAL/Wrapper Function
-echo -e "${FUNC_NAME_LIST}" | while read func
+for func in $(echo "${FUNC_NAME_LIST}")
 do
-    [[ ! ${func} ]] && return
+    # echo ${func}
+    if [ "${func}" = "" ];then
+        continue
+    fi
 
-    FUNC_DEC=$(find ./${OUTPUT_DIR}/eng -name *wrapper.h | xargs -i cat {} 2>/dev/null | sed -n '/.*'$func'(.*/{/.*);/ba;{:c;N;/.*);/!bc};:a;p;q}')
+    FUNC_DEC=$(find ./${OUTPUT_DIR}/eng -name *wrapper.h | xargs -i cat {})
+    FUNC_DEC=$(echo "${FUNC_DEC}" | sed -n '/.*'${func}'(.*/{/.*);/ba;{:c;N;/.*);/!bc};:a;p;q}')
+    
     DATA_TYPE=$(echo "${FUNC_DEC}" | head -1 | awk -F'wrapper|HAL' '{print $1}' | sed s/[[:space:]]//g)
     # echo "${func}"
 
     sed -n '/'${func}':/{:a;N;/*\//!ba;p}' ${WRAPPER_DOC} | sed -n '1d;p' >> ${WRAPPERS_DIR}/wrapper.c
 
-    if [ "${DATA_TYPE}" == "void" ];then
+    if [ "${DATA_TYPE}" = "void" ];then
         echo "${FUNC_DEC}" | sed -n '/;/{s/;/\n{\n\treturn;\n}\n\n/g};p' >> ${WRAPPERS_DIR}/wrapper.c
     else
         echo "${FUNC_DEC}" | sed -n '/;/{s/;/\n{\n\treturn ('${DATA_TYPE}')1;\n}\n\n/g};p' >> ${WRAPPERS_DIR}/wrapper.c
