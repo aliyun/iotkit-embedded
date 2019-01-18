49c0089d92d5d5fab319b61e397733d449b107e5
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-01-18 11:31:14 +0800

    [xtrc_rapper_rules] update rules
    Signed-off-by: yinghao.cyh <yinghao.cyh@alibaba-inc.com>

--- src/dev_model/iot.mk
+++ src/dev_model/iot.mk
@@ -7,9 +7,12 @@ HDR_REFS		+= src/dev_sign
 DEPENDS         += wrappers
 LDFLAGS         += -liot_sdk -liot_hal -liot_tls
 
-TARGET          := alink-example-solo alink-example-gateway
+LIB_SRCS_PATTERN     	:= *.c */*.c
 
-LIB_SRCS_PATTERN     := *.c */*.c
-LIB_SRCS_EXCLUDE     := examples/alink_example_solo.c examples/alink_example_gateway.c
+TARGET          		:= alink-example-solo
+LIB_SRCS_EXCLUDE     	:= examples/alink_example_solo.c examples/alink_example_gateway.c
 SRCS_alink-example-solo   	+= examples/alink_example_solo.c
 SRCS_alink-example-gateway	+= examples/alink_example_gateway.c
+
+$(call Append_Conditional, TARGET, alink-example-gateway, DEVICE_MODEL_GATEWAY)
+
