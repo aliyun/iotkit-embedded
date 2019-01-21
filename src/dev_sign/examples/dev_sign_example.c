5831559cbcbdd9e546a40423bab0892cebdc1256
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-01-21 14:39:11 +0800

    [dev_sign] add some annotation and update header file

--- src/dev_sign/examples/dev_sign_example.c
+++ src/dev_sign/examples/dev_sign_example.c
@@ -1,10 +1,11 @@
-#include "dev_sign_internal.h"
+#include "dev_sign_api.h"
 
 #define EXAMPLE_PRODUCT_KEY     "a1X2bEnP82z"
 #define EXAMPLE_PRODUCT_SECRET  "7jluWm1zql7bt8qK"
 #define EXAMPLE_DEVICE_NAME     "example1"
 #define EXAMPLE_DEVICE_SECRET   "ga7XA6KdlEeiPXQPpRbAjOZXwG8ydgSe"
 
+/* Implenment this HAL or using "printf" of your own system if you want to print something in example*/
 void HAL_Printf(const char *fmt, ...);
 
 int main(int argc, char *argv[])
@@ -12,8 +13,6 @@ int main(int argc, char *argv[])
     iotx_mqtt_region_types_t region = IOTX_CLOUD_REGION_SHANGHAI;
     iotx_dev_meta_info_t meta;
     iotx_sign_mqtt_t sign_mqtt;
-
-    HAL_Printf("mqtt example\n");
 
     memset(&meta,0,sizeof(iotx_dev_meta_info_t));
     memcpy(meta.product_key,EXAMPLE_PRODUCT_KEY,strlen(EXAMPLE_PRODUCT_KEY));
