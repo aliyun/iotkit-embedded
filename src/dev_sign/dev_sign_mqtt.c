3678b16f9e58d074b5d87b8451a90e834e8a1b59
yusheng <yusheng.yx@alibaba-inc.com>
2019-01-21 15:08:34 +0800

    [dev-sign] format dev_sign_mqtt.c coding style
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- src/dev_sign/dev_sign_mqtt.c
+++ src/dev_sign/dev_sign_mqtt.c
@@ -14,9 +14,9 @@
 #define MODE_TCP_DIRECT_PLAIN       "3"
 
 #ifdef SUPPORT_TLS
-#define SECURE_MODE                 MODE_TLS_DIRECT
+    #define SECURE_MODE                 MODE_TLS_DIRECT
 #else
-#define SECURE_MODE                 MODE_TCP_DIRECT_PLAIN
+    #define SECURE_MODE                 MODE_TCP_DIRECT_PLAIN
 #endif
 
 /* use fixed timestamp */
@@ -62,8 +62,9 @@ static int _sign_get_clientid(char *clientid_string, char *device_id)
     memcpy(clientid_string, device_id, strlen(device_id));
     memcpy(clientid_string + strlen(clientid_string), "|", 1);
 
-    for (i=0; i<(sizeof(clientid_kv)/(sizeof(clientid_kv[0]))); i++) {
-        if ( (strlen(clientid_string) + strlen(clientid_kv[i][0]) + strlen(clientid_kv[i][1]) + 2) >= DEV_SIGN_CLIENT_ID_MAXLEN) {
+    for (i = 0; i < (sizeof(clientid_kv) / (sizeof(clientid_kv[0]))); i++) {
+        if ((strlen(clientid_string) + strlen(clientid_kv[i][0]) + strlen(clientid_kv[i][1]) + 2) >=
+            DEV_SIGN_CLIENT_ID_MAXLEN) {
             return FAIL_RETURN;
         }
 
@@ -91,27 +92,29 @@ int32_t IOT_Sign_MQTT(iotx_mqtt_region_types_t region, iotx_dev_meta_info_t *met
         return -1;
     }
 
-    memset(signout,0,sizeof(iotx_sign_mqtt_t));
+    memset(signout, 0, sizeof(iotx_sign_mqtt_t));
 
-    memcpy(device_id,meta->product_key,strlen(meta->product_key));
-    memcpy(device_id+strlen(device_id),".",strlen("."));
-    memcpy(device_id+strlen(device_id),meta->device_name,strlen(meta->device_name));
+    memcpy(device_id, meta->product_key, strlen(meta->product_key));
+    memcpy(device_id + strlen(device_id), ".", strlen("."));
+    memcpy(device_id + strlen(device_id), meta->device_name, strlen(meta->device_name));
 
-    signsource_len = strlen(sign_fmt) + strlen(device_id) + strlen(meta->device_name) + strlen(meta->product_key) + strlen(TIMESTAMP_VALUE) + 1;
+    signsource_len = strlen(sign_fmt) + strlen(device_id) + strlen(meta->device_name) + strlen(meta->product_key) + strlen(
+                                 TIMESTAMP_VALUE) + 1;
     if (signsource_len >= DEV_SIGN_SOURCE_MAXLEN) {
         return ERROR_DEV_SIGN_SOURCE_TOO_SHORT;
     }
-    memset(signsource,0,DEV_SIGN_SOURCE_MAXLEN);
-    memcpy(signsource,"clientId",strlen("clientId"));
-    memcpy(signsource+strlen(signsource),device_id,strlen(device_id));
-    memcpy(signsource+strlen(signsource),"deviceName",strlen("deviceName"));
-    memcpy(signsource+strlen(signsource),meta->device_name,strlen(meta->device_name));
-    memcpy(signsource+strlen(signsource),"productKey",strlen("productKey"));
-    memcpy(signsource+strlen(signsource),meta->product_key,strlen(meta->product_key));
-    memcpy(signsource+strlen(signsource),"timestamp",strlen("timestamp"));
-    memcpy(signsource+strlen(signsource),TIMESTAMP_VALUE,strlen(TIMESTAMP_VALUE));
+    memset(signsource, 0, DEV_SIGN_SOURCE_MAXLEN);
+    memcpy(signsource, "clientId", strlen("clientId"));
+    memcpy(signsource + strlen(signsource), device_id, strlen(device_id));
+    memcpy(signsource + strlen(signsource), "deviceName", strlen("deviceName"));
+    memcpy(signsource + strlen(signsource), meta->device_name, strlen(meta->device_name));
+    memcpy(signsource + strlen(signsource), "productKey", strlen("productKey"));
+    memcpy(signsource + strlen(signsource), meta->product_key, strlen(meta->product_key));
+    memcpy(signsource + strlen(signsource), "timestamp", strlen("timestamp"));
+    memcpy(signsource + strlen(signsource), TIMESTAMP_VALUE, strlen(TIMESTAMP_VALUE));
 
-    utils_hmac_sha256((uint8_t *)signsource,strlen(signsource),(uint8_t *)meta->device_secret,strlen(meta->device_secret),sign);
+    utils_hmac_sha256((uint8_t *)signsource, strlen(signsource), (uint8_t *)meta->device_secret,
+                      strlen(meta->device_secret), sign);
 
     /* Get Sign Information For MQTT */
     length = strlen(meta->product_key) + strlen(g_infra_mqtt_domain[region].region) + 2;
@@ -119,23 +122,24 @@ int32_t IOT_Sign_MQTT(iotx_mqtt_region_types_t region, iotx_dev_meta_info_t *met
         return ERROR_DEV_SIGN_HOST_NAME_TOO_SHORT;
     }
 
-    memset(signout->hostname,0,DEV_SIGN_HOSTNAME_MAXLEN);
-    memcpy(signout->hostname,meta->product_key,strlen(meta->product_key));
-    memcpy(signout->hostname+strlen(signout->hostname),".",strlen("."));
-    memcpy(signout->hostname+strlen(signout->hostname),g_infra_mqtt_domain[region].region,strlen(g_infra_mqtt_domain[region].region));
+    memset(signout->hostname, 0, DEV_SIGN_HOSTNAME_MAXLEN);
+    memcpy(signout->hostname, meta->product_key, strlen(meta->product_key));
+    memcpy(signout->hostname + strlen(signout->hostname), ".", strlen("."));
+    memcpy(signout->hostname + strlen(signout->hostname), g_infra_mqtt_domain[region].region,
+           strlen(g_infra_mqtt_domain[region].region));
 
     length = strlen(meta->device_name) + strlen(meta->product_key) + 2;
     if (length >= DEV_SIGN_USERNAME_MAXLEN) {
         return ERROR_DEV_SIGN_USERNAME_TOO_SHORT;
     }
 
-    memset(signout->username,0,DEV_SIGN_USERNAME_MAXLEN);
-    memcpy(signout->username,meta->device_name,strlen(meta->device_name));
-    memcpy(signout->username+strlen(signout->username),"&",strlen("&"));
-    memcpy(signout->username+strlen(signout->username),meta->product_key,strlen(meta->product_key));
+    memset(signout->username, 0, DEV_SIGN_USERNAME_MAXLEN);
+    memcpy(signout->username, meta->device_name, strlen(meta->device_name));
+    memcpy(signout->username + strlen(signout->username), "&", strlen("&"));
+    memcpy(signout->username + strlen(signout->username), meta->product_key, strlen(meta->product_key));
 
-    memset(signout->password,0,DEV_SIGN_PASSWORD_MAXLEN);
-    infra_hex2str(sign,32,signout->password);
+    memset(signout->password, 0, DEV_SIGN_PASSWORD_MAXLEN);
+    infra_hex2str(sign, 32, signout->password);
 
     if (_sign_get_clientid(signout->clientid, device_id) != SUCCESS_RETURN) {
         return ERROR_DEV_SIGN_CLIENT_ID_TOO_SHORT;
