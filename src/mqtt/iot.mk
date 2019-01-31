2c7cb10feffe5cd4cdca341e1f81b8eed96426fc
yusheng <yusheng.yx@alibaba-inc.com>
2019-01-31 19:59:59 +0800

    [src/mqtt] remove special LDFLAGS setting when AT_PARSER_ENABLED
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- src/mqtt/iot.mk
+++ src/mqtt/iot.mk
@@ -9,8 +9,4 @@ $(call Append_Conditional, LIB_SRCS_PATTERN, mqtt_impl/*.c, MQTT_DEFAULT_IMPL)
 $(call Append_Conditional, TARGET, mqtt-example, MQTT_COMM_ENABLED, ATM_ENABLED)
 $(call Append_Conditional, TARGET, mqtt-example-at, ATM_ENABLED)
 
-ifneq (,$(filter -DAT_PARSER_ENABLED,$(CFLAGS)))
-LDFLAGS         += -liot_sdk -liot_hal -liot_sdk -liot_hal -liot_tls
-else
 LDFLAGS         += -liot_sdk -liot_hal -liot_tls
-endif
