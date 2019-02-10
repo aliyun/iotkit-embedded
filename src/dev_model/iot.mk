d7bc343c71cf812c5ac03003624d1686e66f1f7a
yusheng <yusheng.yx@alibaba-inc.com>
2019-02-10 17:02:04 +0800

    [dev_model] fix incorrect including cJSON.c into libiot_dm.a mistake
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- src/dev_model/iot.mk
+++ src/dev_model/iot.mk
@@ -9,11 +9,12 @@ LDFLAGS         += -liot_sdk -liot_hal -liot_tls
 
 LIB_SRCS_PATTERN     	:= *.c server/*.c client/*.c
 
-LIB_SRCS_EXCLUDE     	  := examples/linkkit_example_solo.c
-SRCS_linkkit-example-solo := examples/linkkit_example_solo.c
+LIB_SRCS_EXCLUDE     	  := examples/linkkit_example_solo.c cJSON.c
+SRCS_linkkit-example-solo := examples/linkkit_example_solo.c cJSON.c
 
 LIB_SRCS_EXCLUDE             += examples/linkkit_example_gateway.c
-SRCS_linkkit-example-gateway = examples/linkkit_example_gateway.c
+SRCS_linkkit-example-gateway := examples/linkkit_example_gateway.c
 
 $(call Append_Conditional, TARGET, linkkit-example-solo, DEVICE_MODEL_ENABLED)
 $(call Append_Conditional, TARGET, linkkit-example-gateway, DEVICE_MODEL_GATEWAY)
+
