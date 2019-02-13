3f3bef07abd4a50a278dd702c7a87a4091b2f376
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-02-13 17:06:53 +0800

    [dev_model] fix alcs issue

--- src/dev_model/alcs/alcs_base64.c
+++ src/dev_model/alcs/alcs_base64.c
@@ -39,7 +39,7 @@ static void build_decoding_table()
     return;
 }
 
-iotx_err_t utils_base64encode(const uint8_t *data, uint32_t inputLength, uint32_t outputLenMax,
+int utils_base64encode(const uint8_t *data, uint32_t inputLength, uint32_t outputLenMax,
                               uint8_t *encodedData, uint32_t *outputLength)
 {
     uint32_t i = 0;
@@ -75,7 +75,7 @@ iotx_err_t utils_base64encode(const uint8_t *data, uint32_t inputLength, uint32_
     return SUCCESS_RETURN;
 }
 
-iotx_err_t utils_base64decode(const uint8_t *data, uint32_t inputLength, uint32_t outputLenMax,
+int utils_base64decode(const uint8_t *data, uint32_t inputLength, uint32_t outputLenMax,
                               uint8_t *decodedData, uint32_t *outputLength)
 {
     uint32_t i = 0;
