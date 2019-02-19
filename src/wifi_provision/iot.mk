b44e6441c25b3c65ec45a9a1242e3af7c0b68b2e
xicai.cxc <xicai.cxc@alibaba-inc.com>
2019-02-19 17:24:52 +0800

    add dependency relation into config files to avoid build error

--- src/wifi_provision/iot.mk
+++ src/wifi_provision/iot.mk
@@ -8,5 +8,5 @@ $(call Append_Conditional, LIB_SRCS_PATTERN, zero-config/*.c, AWSS_SUPPORT_ZEROC
 $(call Append_Conditional, LIB_SRCS_PATTERN, phone-ap/*.c, AWSS_SUPPORT_AHA)
 $(call Append_Conditional, LIB_SRCS_PATTERN, router-ap/*.c, AWSS_SUPPORT_ADHA)
 $(call Append_Conditional, LIB_SRCS_PATTERN, dev-ap/*.c, AWSS_SUPPORT_DEV_AP)
-$(call Append_Conditional, LIB_SRCS_PATTERN, frameworks/*.c, WIFI_PROVISION_ENABLED)
-$(call Append_Conditional, LIB_SRCS_PATTERN, frameworks/*/*.c, WIFI_PROVISION_ENABLED)
+$(call Append_Conditional, LIB_SRCS_PATTERN, frameworks/*.c, AWSS_FRAMEWORKS)
+$(call Append_Conditional, LIB_SRCS_PATTERN, frameworks/*/*.c, AWSS_FRAMEWORKS)
