c61e855fcddcc2676cc1e3b77dc70391ce1addb5
yusheng <yusheng.yx@alibaba-inc.com>
2019-02-28 15:31:34 +0800

    [guider] fix log connectting http but HTTPS in fact mistake
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- src/infra/system/guider.c
+++ src/infra/system/guider.c
@@ -305,7 +305,7 @@ static void guider_get_url(char *buf, int len)
 #ifdef MQTT_DIRECT
     HAL_Snprintf(buf, len, "%s", "");
 #else
-    HAL_Snprintf(buf, len, "%s", "http://");
+    HAL_Snprintf(buf, len, "%s", "https://");
 
     switch (g_domain_type) {
         case GUIDER_DOMAIN_SH:
