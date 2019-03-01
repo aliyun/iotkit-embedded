d29fc2f87650ea9611c7c7cea17aff5fb94a9b39
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-03-01 11:40:10 +0800

    [dev_reset] improve IOT_DevReset_Report api

--- src/dev_reset/dev_reset.c
+++ src/dev_reset/dev_reset.c
@@ -9,8 +9,33 @@ extern "C" {
 #endif
 
 static int g_dev_reset_sub_flag = 0;
+iotx_devrst_evt_handle_t g_devrst_event_handle = NULL;
 
-int IOT_DevReset_Report(iotx_dev_meta_info_t *meta_info, iotx_mqtt_event_handle_func_fpt handle, void *extended)
+
+#include "mqtt_api.h"
+static void mqtt_sub_handle(void *pcontext, void *pclient, iotx_mqtt_event_msg_pt msg)
+{
+    switch (msg->event_type)
+    {
+        case IOTX_MQTT_EVENT_PUBLISH_RECEIVED: {
+            iotx_mqtt_topic_info_t *packet_info = (iotx_mqtt_topic_info_t *)msg->msg;
+            if (g_devrst_event_handle) {
+                iotx_devrst_evt_recv_msg_t recv_msg;
+                memset(&recv_msg, 0, sizeof(iotx_devrst_evt_recv_msg_t));
+                recv_msg.msgid = packet_info->packet_id;
+                recv_msg.payload = (char *)packet_info->payload;
+                recv_msg.payload_len = packet_info->payload_len;
+                g_devrst_event_handle(IOTX_DEVRST_EVT_RECEIVED, (void *)&recv_msg);
+            }
+        }
+        break;
+
+        default:
+            break;
+    }
+}
+
+int IOT_DevReset_Report(iotx_dev_meta_info_t *meta_info, iotx_devrst_evt_handle_t handle, void *extended)
 {
     int res = 0;
     const char *reset_fmt = "/sys/%s/%s/thing/reset";
@@ -22,12 +47,13 @@ int IOT_DevReset_Report(iotx_dev_meta_info_t *meta_info, iotx_mqtt_event_handle_
     if (meta_info == NULL || handle== NULL ) {
         return FAIL_RETURN;
     }
+    g_devrst_event_handle = handle;
 
     memset(topic, 0, IOTX_PRODUCT_KEY_LEN + IOTX_DEVICE_NAME_LEN + 30);
     HAL_Snprintf(topic,IOTX_PRODUCT_KEY_LEN + IOTX_DEVICE_NAME_LEN + 30, reset_reply_fmt, meta_info->product_key, meta_info->device_name);
 
     if (g_dev_reset_sub_flag == 0) {
-        res = IOT_MQTT_Subscribe_Sync(NULL, topic, IOTX_MQTT_QOS0, handle, NULL, 5000);
+        res = IOT_MQTT_Subscribe_Sync(NULL, topic, IOTX_MQTT_QOS0, mqtt_sub_handle, NULL, 5000);
         if (res < 0 ) {
             return FAIL_RETURN;
         }
--- src/dev_reset/dev_reset_api.h
+++ src/dev_reset/dev_reset_api.h
@@ -3,7 +3,18 @@
 
 #include "infra_types.h"
 #include "infra_defs.h"
-#include "mqtt_api.h"
+
+typedef enum {
+    IOTX_DEVRST_EVT_RECEIVED
+} iotx_devrst_evt_type_t;
+
+typedef struct {
+    int msgid;
+    char *payload;
+    uint32_t payload_len;
+} iotx_devrst_evt_recv_msg_t;
+
+typedef void (*iotx_devrst_evt_handle_t)(iotx_devrst_evt_type_t evt, void *msg);
 
 /**
  * @brief   report reset message to cloud.
@@ -14,7 +25,7 @@
  * @retval  -1 : failure
  * @retval  0 : sucess
  */
-int IOT_DevReset_Report(iotx_dev_meta_info_t *meta_info, iotx_mqtt_event_handle_func_fpt handle, void *extended);
+int IOT_DevReset_Report(iotx_dev_meta_info_t *meta_info, iotx_devrst_evt_handle_t handle, void *extended);
 
 #endif
 
