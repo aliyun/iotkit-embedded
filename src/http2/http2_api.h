1d12c929447b4fe7ffccf3a5be1ca7d64ee79929
yinghao.cyh <yinghao.cyh@alibaba-inc.com>
2019-03-05 16:06:39 +0800

    [http2] use independent upload_api src/head file
    Signed-off-by: yinghao.cyh <yinghao.cyh@alibaba-inc.com>

--- src/http2/http2_api.h
+++ src/http2/http2_api.h
@@ -90,44 +90,6 @@ typedef struct {
     void                *user_data;         /* user data brought in at stream open */
 } stream_data_info_t;
 
-#ifdef FS_ENABLED
-/* bit define of file override option */
-#define UPLOAD_FILE_OPT_BIT_OVERWRITE       (0x00000001)
-#define UPLOAD_FILE_OPT_BIT_RESUME          (0x00000002)
-#define UPLOAD_FILE_OPT_BIT_CRC64           (0x00000004)
-#define UPLOAD_FILE_OPT_BIT_SPECIFIC_LEN    (0x00000008)
-
-/* file upload option define */
-typedef struct {
-    const char *file_path;
-    const char *upload_id;      /* a specific id used to indicate one upload session, only required when UPLOAD_FILE_OPT_BIT_RESUME option set */
-    uint32_t upload_len;        /* used to indicate the upload length, only required when UPLOAD_FILE_OPT_BIT_SPECIFIC_LEN option set */
-    uint32_t opt_bit_map;
-} http2_file_upload_params_t;
-
-/* error code for file upload */
-typedef enum {
-    UPLOAD_STOP_BY_IOCTL        = -14,
-    UPLOAD_HTTP2_HANDLE_NULL    = -13,
-    UPLOAD_LEN_IS_ZERO          = -12,
-    UPLOAD_FILE_PATH_IS_NULL    = -11,
-    UPLOAD_ID_IS_NULL           = -10,
-    UPLOAD_FILE_NOT_EXIST     = -9,
-    UPLOAD_FILE_READ_FAILED   = -8,
-    UPLOAD_STREAM_OPEN_FAILED = -7,
-    UPLOAD_STREAM_SEND_FAILED = -6,
-    UPLOAD_MALLOC_FAILED      = -5,
-    UPLOAD_NULL_POINT         = -2,
-    UPLOAD_ERROR_COMMON       = -1,
-    UPLOAD_SUCCESS            = 0,
-} http2_file_upload_result_t;
-
-/* callback function type define */
-typedef void (* http2_file_upload_cb_t)(const char *file_path, const char *upload_id, int result, void *user_data);
-
-DLL_IOT_API int IOT_HTTP2_Stream_UploadFile(void *http2_handle, http2_file_upload_params_t *params, http2_file_upload_cb_t cb, void *user_data);
-#endif /* #ifdef FS_ENABLED */
-
 DLL_IOT_API void *IOT_HTTP2_Connect(device_conn_info_t *conn_info, http2_stream_cb_t *user_cb);
 DLL_IOT_API int IOT_HTTP2_Stream_Open(void *handle, stream_data_info_t *info, header_ext_info_t *header);
 DLL_IOT_API int IOT_HTTP2_Stream_Send(void *handle, stream_data_info_t *info, header_ext_info_t *header);
