1920c278e99f1bc1800cc77d4ce274604aa37cbc
yusheng <yusheng.yx@alibaba-inc.com>
2019-03-05 19:46:40 +0800

    [[wrapper] only keep valid implementation code in HAL_OS_Freertos.c
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- wrappers/os/freertos/HAL_OS_Freertos.c
+++ wrappers/os/freertos/HAL_OS_Freertos.c
@@ -1,29 +1,14 @@
-/**
- * NOTE:
- *
- * HAL_TCP_xxx API reference implementation: wrappers/os/ubuntu/HAL_TCP_linux.c
- *
+/*
+ * Copyright (C) 2015-2018 Alibaba Group Holding Limited
  */
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
-#include "stm32f1xx_hal.h"
-#include "cmsis_os.h"
 
 #include "infra_types.h"
 #include "infra_defs.h"
 #include "wrappers_defs.h"
-#include "at_wrapper.h"
-
-#define EXAMPLE_PRODUCT_KEY         "a1X2bEnP82z"
-#define EXAMPLE_PRODUCT_SECRET      "7jluWm1zql7bt8qK"
-#define EXAMPLE_DEVICE_NAME         "example1"
-#define EXAMPLE_DEVICE_SECRET       "ga7XA6KdlEeiPXQPpRbAjOZXwG8ydgSe"
-
-#define EXAMPLE_FIRMWARE_VERSION    "app-1.0.0-20190118.1000"
-
-#define RING_BUFFER_SIZE            (128)
-
 
 #define HAL_SEM_MAX_COUNT           (10)
 #define HAL_SEM_INIT_COUNT          (0)
@@ -31,157 +16,6 @@
 #define DEFAULT_THREAD_NAME         "linkkit_task"
 #define DEFAULT_THREAD_SIZE         (256)
 #define TASK_STACK_ALIGN_SIZE       (4)
-
-
-typedef struct
-{
-  uint8_t  data[RING_BUFFER_SIZE];
-  uint16_t tail; 
-  uint16_t head;
-}uart_ring_buffer_t;
-
-extern UART_HandleTypeDef huart1;
-
-static uint8_t  g_uart_tx_flag;
-static uart_ring_buffer_t   g_uart_rx_buf;
-static UART_HandleTypeDef* p_at_uart = &huart1;
-
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-    if (++g_uart_rx_buf.tail >= RING_BUFFER_SIZE){
-        g_uart_rx_buf.tail = 0;   
-    }
-    HAL_UART_Receive_IT(p_at_uart, (uint8_t *)&g_uart_rx_buf.data[g_uart_rx_buf.tail], 1);
-}
-
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
-{
-    g_uart_tx_flag = 1;
-}
-
-/**
- * Deinitialises a UART interface
- *
- * @param[in]  uart  the interface which should be deinitialised
- *
- * @return  0 : on success, EIO : if an error occurred with any step
- */
-int32_t HAL_AT_Uart_Deinit(uart_dev_t *uart)
-{
-    HAL_StatusTypeDef ret;
-    ret = HAL_UART_DeInit(p_at_uart);
-    if(HAL_OK != ret){
-        return -1;
-    }
-    return 0;
-}
-
-
-/**
- * Initialises a UART interface
- *
- *
- * @param[in]  uart  the interface which should be initialised
- *
- * @return  0 : on success, EIO : if an error occurred with any step
- */
-int32_t HAL_AT_Uart_Init(uart_dev_t *uart)
-{
-    char c;
-    HAL_StatusTypeDef ret;
-    if (uart == NULL) {
-        return -1;
-    }
-
-    g_uart_rx_buf.head = 0;
-    g_uart_rx_buf.tail = 0;
-    ret = HAL_UART_Receive_IT(p_at_uart, (uint8_t *)&c, 1);
-    if(HAL_OK != ret){
-        return -1;
-    }
-    return 0;
-}
-
-
-/**
- * Receive data on a UART interface
- *
- * @param[in]   uart         the UART interface
- * @param[out]  data         pointer to the buffer which will store incoming data
- * @param[in]   expect_size  number of bytes to receive
- * @param[out]  recv_size    number of bytes received
- * @param[in]   timeout      timeout in milisecond, set this value to HAL_WAIT_FOREVER
- *                           if you want to wait forever
- *
- * @return  0 : on success, EIO : if an error occurred with any step
- */
-int32_t HAL_AT_Uart_Recv(uart_dev_t *uart, void *data, uint32_t expect_size,
-                         uint32_t *recv_size, uint32_t timeout)
-{
-    uint32_t ReadData = 0;
-    uint8_t *buf = (uint8_t *)data;
-    
-    /* Loop until data received */
-    while (expect_size--)
-    {
-      uint32_t tickStart = xTaskGetTickCount();
-      do
-      {
-        if(g_uart_rx_buf.head != g_uart_rx_buf.tail)
-        {
-          /* serial data available, so return data to user */
-          *buf++ = g_uart_rx_buf.data[g_uart_rx_buf.head++];
-          ReadData++;
-    
-          /* check for ring buffer wrap */
-          if (g_uart_rx_buf.head >= RING_BUFFER_SIZE)
-          {
-            /* Ring buffer wrap, so reset head pointer to start of buffer */
-            g_uart_rx_buf.head = 0;
-          }
-          break;
-        }
-      }while((xTaskGetTickCount() - tickStart ) < timeout);
-    }
-    
-    *recv_size = ReadData;
-    
-    return 0;
-}
-
-
-/**
- * Transmit data on a UART interface
- *
- * @param[in]  uart     the UART interface
- * @param[in]  data     pointer to the start of data
- * @param[in]  size     number of bytes to transmit
- * @param[in]  timeout  timeout in milisecond, set this value to HAL_WAIT_FOREVER
- *                      if you want to wait forever
- *
- * @return  0 : on success, EIO : if an error occurred with any step
- */
-int32_t HAL_AT_Uart_Send(uart_dev_t *uart, const void *data, uint32_t size, uint32_t timeout)
-{
-    uint32_t tickstart;
-    g_uart_tx_flag = 0;
-    
-    if (HAL_UART_Transmit_IT(p_at_uart, (uint8_t*)data, size) != HAL_OK)
-    {
-       return -1;
-    }
-    
-    tickstart = xTaskGetTickCount();
-    while(g_uart_tx_flag!=1)
-    {
-      if((xTaskGetTickCount()-tickstart) > timeout) {
-        return -1;
-      }
-    }
-    
-    return 0;
-}
-
 
 /**
  * @brief Deallocate memory block
@@ -194,81 +28,6 @@ int32_t HAL_AT_Uart_Send(uart_dev_t *uart, const void *data, uint32_t size, uint
 void HAL_Free(void *ptr)
 {
     vPortFree(ptr);
-}
-
-
-/**
- * @brief Get device name from user's system persistent storage
- *
- * @param [ou] device_name: array to store device name, max length is IOTX_DEVICE_NAME_LEN
- * @return the actual length of device name
- */
-int HAL_GetDeviceName(char device_name[IOTX_DEVICE_NAME_LEN])
-{
-    int len = strlen(EXAMPLE_DEVICE_NAME);
-    if(len >= IOTX_DEVICE_NAME_LEN){
-        return -1;
-    }
-    memset(device_name, 0x0, IOTX_DEVICE_NAME_LEN); 
-    strncpy(device_name, EXAMPLE_DEVICE_NAME, len); 
-    return strlen(device_name);
-}
-
-
-/**
- * @brief Get device secret from user's system persistent storage
- *
- * @param [ou] device_secret: array to store device secret, max length is IOTX_DEVICE_SECRET_LEN
- * @return the actual length of device secret
- */
-int HAL_GetDeviceSecret(char device_secret[IOTX_DEVICE_SECRET_LEN])
-{
-    int len = strlen(EXAMPLE_DEVICE_SECRET); 
-    if(len >= IOTX_DEVICE_SECRET_LEN){
-        return -1;
-    }
-    
-    memset(device_secret, 0x0, IOTX_DEVICE_SECRET_LEN); 
-    strncpy(device_secret, EXAMPLE_DEVICE_SECRET, len); 
-    return len;
-}
-
-
-/**
- * @brief Get firmware version
- *
- * @param [ou] version: array to store firmware version, max length is IOTX_FIRMWARE_VER_LEN
- * @return the actual length of firmware version
- */
-int HAL_GetFirmwareVersion(char *version)
-{
-    int len = strlen(EXAMPLE_FIRMWARE_VERSION);
-    if(len >= IOTX_FIRMWARE_VER_LEN-1){
-        return -1;
-    }
-    memset(version, 0x0, IOTX_FIRMWARE_VER_LEN);
-    strncpy(version, EXAMPLE_FIRMWARE_VERSION, len);
-    version[len] = '\0';
-    return strlen(version);
-
-}
-
-
-/**
- * @brief Get product key from user's system persistent storage
- *
- * @param [ou] product_key: array to store product key, max length is IOTX_PRODUCT_KEY_LEN
- * @return  the actual length of product key
- */
-int HAL_GetProductKey(char product_key[IOTX_PRODUCT_KEY_LEN])
-{
-    int len = strlen(EXAMPLE_PRODUCT_KEY); 
-    if(len >= IOTX_PRODUCT_KEY_LEN){
-        return -1;
-    }
-    memset(product_key, 0x0, IOTX_PRODUCT_KEY_LEN); 
-    strncpy(product_key, EXAMPLE_PRODUCT_KEY, len); 
-    return len;
 }
 
 /**
@@ -284,7 +43,6 @@ void *HAL_Malloc(uint32_t size)
     return pvPortMalloc(size);
 }
 
-
 /**
  * @brief Create a mutex.
  *
@@ -298,13 +56,12 @@ void *HAL_MutexCreate(void)
     QueueHandle_t sem;
 
     sem = xSemaphoreCreateMutex();
-    if (0 == sem){
+    if (0 == sem) {
         return NULL;
     }
 
     return sem;
 }
-
 
 /**
  * @brief Destroy the specified mutex object, it will release related resource.
@@ -315,15 +72,14 @@ void *HAL_MutexCreate(void)
  * @note None.
  */
 void HAL_MutexDestroy(void *mutex)
-{   
+{
     QueueHandle_t sem;
-    if(mutex == NULL){
+    if (mutex == NULL) {
         return;
     }
-    sem = (QueueHandle_t )mutex;
+    sem = (QueueHandle_t)mutex;
     vSemaphoreDelete(sem);
 }
-
 
 /**
  * @brief Waits until the specified mutex is in the signaled state.
@@ -337,17 +93,16 @@ void HAL_MutexLock(void *mutex)
 {
     BaseType_t ret;
     QueueHandle_t sem;
-    if(mutex == NULL){
+    if (mutex == NULL) {
         return;
     }
-    
+
     sem = (QueueHandle_t)mutex;
-    ret = xSemaphoreTake(sem,0xffffffff);
-    while(pdPASS != ret){
+    ret = xSemaphoreTake(sem, 0xffffffff);
+    while (pdPASS != ret) {
         ret = xSemaphoreTake(sem, 0xffffffff);
     }
 }
-
 
 /**
  * @brief Releases ownership of the specified mutex object..
@@ -360,13 +115,12 @@ void HAL_MutexLock(void *mutex)
 void HAL_MutexUnlock(void *mutex)
 {
     QueueHandle_t sem;
-    if(mutex == NULL){
+    if (mutex == NULL) {
         return;
     }
     sem = (QueueHandle_t)mutex;
-    (void)xSemaphoreGive(sem);    
+    (void)xSemaphoreGive(sem);
 }
-
 
 /**
  * @brief Writes formatted data to stream.
@@ -380,13 +134,12 @@ void HAL_MutexUnlock(void *mutex)
  */
 void HAL_Printf(const char *fmt, ...)
 {
-    va_list args; 
-    va_start(args, fmt); 
-    vprintf(fmt, args); 
-    va_end(args); 
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
     fflush(stdout);
 }
-
 
 /**
  * @brief   create a semaphore
@@ -397,15 +150,14 @@ void HAL_Printf(const char *fmt, ...)
  */
 void *HAL_SemaphoreCreate(void)
 {
-    QueueHandle_t sem = 0; 
-    sem = xSemaphoreCreateCounting(HAL_SEM_MAX_COUNT,HAL_SEM_INIT_COUNT);
-    if (0 == sem){
+    QueueHandle_t sem = 0;
+    sem = xSemaphoreCreateCounting(HAL_SEM_MAX_COUNT, HAL_SEM_INIT_COUNT);
+    if (0 == sem) {
         return NULL;
     }
-    
+
     return sem;
 }
-
 
 /**
  * @brief   destory a semaphore
@@ -419,14 +171,13 @@ void HAL_SemaphoreDestroy(void *sem)
 {
     QueueHandle_t queue;
 
-    if(sem == NULL){
+    if (sem == NULL) {
         return;
     }
-    queue = (QueueHandle_t )sem;
-    
+    queue = (QueueHandle_t)sem;
+
     vSemaphoreDelete(queue);
 }
-
 
 /**
  * @brief   signal thread wait on a semaphore
@@ -439,11 +190,11 @@ void HAL_SemaphoreDestroy(void *sem)
 void HAL_SemaphorePost(void *sem)
 {
     QueueHandle_t queue;
-    if(sem == NULL){
+    if (sem == NULL) {
         return;
     }
     queue = (QueueHandle_t)sem;
-    (void)xSemaphoreGive(queue);    
+    (void)xSemaphoreGive(queue);
 }
 
 
@@ -465,18 +216,17 @@ int HAL_SemaphoreWait(void *sem, uint32_t timeout_ms)
 {
     BaseType_t ret = 0;
     QueueHandle_t queue;
-    if(sem == NULL){
+    if (sem == NULL) {
         return -1;
     }
-    
+
     queue = (QueueHandle_t)sem;
-    ret = xSemaphoreTake(queue,timeout_ms);
-    if(pdPASS != ret){
+    ret = xSemaphoreTake(queue, timeout_ms);
+    if (pdPASS != ret) {
         return -1;
     }
     return 0;
 }
-
 
 /**
  * @brief Sleep thread itself.
@@ -490,7 +240,6 @@ void HAL_SleepMs(uint32_t ms)
 {
     osDelay(ms);
 }
-
 
 /**
  * @brief Writes formatted data to string.
@@ -506,12 +255,12 @@ void HAL_SleepMs(uint32_t ms)
  */
 int HAL_Snprintf(char *str, const int len, const char *fmt, ...)
 {
-    va_list args; 
-    int rc; 
+    va_list args;
+    int rc;
 
-    va_start(args, fmt); 
-    rc = vsnprintf(str, len, fmt, args); 
-    va_end(args); 
+    va_start(args, fmt);
+    rc = vsnprintf(str, len, fmt, args);
+    va_end(args);
     return rc;
 }
 
@@ -534,38 +283,37 @@ int HAL_Snprintf(char *str, const int len, const char *fmt, ...)
  * @note None.
  */
 int HAL_ThreadCreate(
-                     void **thread_handle,
-                     void *(*work_routine)(void *),
-                     void *arg,
-                     hal_os_thread_param_t *hal_os_thread_param,
-                     int *stack_used)
+            void **thread_handle,
+            void *(*work_routine)(void *),
+            void *arg,
+            hal_os_thread_param_t *hal_os_thread_param,
+            int *stack_used)
 {
     char *name;
     size_t stacksize;
-    //osPriority prio;
     osThreadDef_t thread_def;
-    
+
     osThreadId handle;
-    
-    if (thread_handle == NULL){
+
+    if (thread_handle == NULL) {
         return -1;
     }
-    
-    if (work_routine == NULL){
+
+    if (work_routine == NULL) {
         return -1;
     }
-    
-    if (hal_os_thread_param == NULL){
+
+    if (hal_os_thread_param == NULL) {
         return -1;
     }
-    if (stack_used == NULL){
+    if (stack_used == NULL) {
         return -1;
     }
-    
+
     if (stack_used != NULL) {
         *stack_used = 0;
     }
-    
+
     if (!hal_os_thread_param->name) {
         name = DEFAULT_THREAD_NAME;
     } else {
@@ -582,16 +330,15 @@ int HAL_ThreadCreate(
     thread_def.pthread = (os_pthread)work_routine;
     thread_def.tpriority = (osPriority)hal_os_thread_param->priority;
     thread_def.instances = 0;
-    thread_def.stacksize = (stacksize + TASK_STACK_ALIGN_SIZE-1)/TASK_STACK_ALIGN_SIZE;
+    thread_def.stacksize = (stacksize + TASK_STACK_ALIGN_SIZE - 1) / TASK_STACK_ALIGN_SIZE;
 
     handle = osThreadCreate(&thread_def, arg);
-    if( NULL == handle){
+    if (NULL == handle) {
         return -1;
     }
-    *thread_handle = (void*)handle;
+    *thread_handle = (void *)handle;
     return 0;
 }
-
 
 /**
  * @brief Retrieves the number of milliseconds that have elapsed since the system was boot.
@@ -604,5 +351,4 @@ uint64_t HAL_UptimeMs(void)
 {
     return (uint64_t)xTaskGetTickCount();
 }
-
 
