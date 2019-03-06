a2386a3d30f42f68af281a6c193258219b1a3851
yusheng <yusheng.yx@alibaba-inc.com>
2019-03-06 17:27:27 +0800

    [dynreg] remove HAL_GetXXX() dependecy from dynreg_wrapper.h
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- src/dynamic_register/dynreg_wrapper.h
+++ src/dynamic_register/dynreg_wrapper.h
@@ -7,9 +7,5 @@ void *HAL_Malloc(uint32_t size);
 void HAL_Free(void *ptr);
 void HAL_Printf(const char *fmt, ...);
 
-int HAL_GetProductKey(char product_key[IOTX_PRODUCT_KEY_LEN + 1]);
-int HAL_GetProductSecret(char product_secret[IOTX_PRODUCT_SECRET_LEN + 1]);
-int HAL_GetDeviceName(char device_name[IOTX_DEVICE_NAME_LEN + 1]);
-
 #endif
 
