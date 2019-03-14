e8a5924f1c5141ea5409dc2ee8e9fd2671b05482
yusheng <yusheng.yx@alibaba-inc.com>
2019-03-14 08:20:23 +0800

    [http] remove incorrectly included mqtt_api.h in http_wrapper.h
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- src/http/http_wrapper.h
+++ src/http/http_wrapper.h
@@ -5,14 +5,11 @@
 #include "infra_defs.h"
 #include "wrappers_defs.h"
 
-#include "mqtt_api.h"
-
 void *HAL_Malloc(uint32_t size);
 void HAL_Free(void *ptr);
 void HAL_Printf(const char *fmt, ...);
 int HAL_Snprintf(char *str, const int len, const char *fmt, ...);
 uint64_t HAL_UptimeMs(void);
 void HAL_SleepMs(uint32_t ms);
-
 
 #endif /* #ifndef _HTTP_WRAPPER_H */
