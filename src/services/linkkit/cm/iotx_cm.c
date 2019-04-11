5d6d9119feb0bcb3194137ba9dddc06bb05e87af
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-04-11 15:00:42 +0800

    [cm] fix inited_conn_num issue

--- src/services/linkkit/cm/iotx_cm.c
+++ src/services/linkkit/cm/iotx_cm.c
@@ -208,7 +208,11 @@ int iotx_cm_close(int fd)
         return -1;
     }
 
-    if (--inited_conn_num == 0) {
+    if (inited_conn_num > 0) {
+        inited_conn_num--;
+    }
+
+    if (inited_conn_num == 0) {
 #if (CONFIG_SDK_THREAD_COST == 1)
         while (!yield_task_leave) {
             HAL_SleepMs(10);
