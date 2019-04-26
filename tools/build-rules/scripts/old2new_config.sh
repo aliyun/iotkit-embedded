5f5ecf77a755efa6dcc36b8bcb8bef6ff7fca86e
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-04-26 19:44:55 +0800

    append FEATURE_SUPPORT_TLS to make.setting when convert

--- tools/build-rules/scripts/old2new_config.sh
+++ tools/build-rules/scripts/old2new_config.sh
@@ -3,6 +3,7 @@
 mv make.settings .config
 sed -i 's/FEATURE_//g;/AWSS_SUPPORT_ROUTER/d;s/AWSS_SUPPORT_PHONEASAP/AWSS_SUPPORT_AHA/g' .config
 tools/prebuilt/ubuntu/bin/kconfig-conf -s --olddefconfig tools/Config.in
-sed -i '/^[A-Z]/{s/^/FEATURE_/g}' .config
+sed -i '/MUTE/d;/^[A-Z]/{s/^/FEATURE_/g}' .config
 sed -i '/^#/{s/^# \([A-Z]\)\(.*is not set\)/# FEATURE_\1\2/g}' .config
+sed -i '$ a\FEATURE_SUPPORT_TLS=y' .config
 mv .config make.settings
