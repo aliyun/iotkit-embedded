22d3a4f6eae88c00da1f84a714620bf8bf7c8f4e
yusheng <yusheng.yx@alibaba-inc.com>
2019-04-26 13:37:17 +0800

    [make.settings] restore to default status
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- tools/build-rules/scripts/old2new_config.sh
+++ tools/build-rules/scripts/old2new_config.sh
@@ -2,6 +2,7 @@
 
 mv make.settings .config
 sed -i 's/FEATURE_//g;/AWSS_SUPPORT_ROUTER/d;s/AWSS_SUPPORT_PHONEASAP/AWSS_SUPPORT_AHA/g' .config
-tools/prebuilt/ubuntu/bin/kconfig-conf --olddefconfig tools/Config.in
+tools/prebuilt/ubuntu/bin/kconfig-conf -s --olddefconfig tools/Config.in
 sed -i '/^[A-Z]/{s/^/FEATURE_/g}' .config
+sed -i '/^#/{s/^# \([A-Z]\)\(.*is not set\)/# FEATURE_\1\2/g}' .config
 mv .config make.settings
