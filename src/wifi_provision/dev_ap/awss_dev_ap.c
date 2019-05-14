736b16580f8f7356d8ccd2a6e6e226ce383ffee9
Xicai Chen <xicai.cxc@alibaba-inc.com>
2019-05-14 11:25:16 +0800

    fix dev_ap while-sleep issue in coap_server

--- src/wifi_provision/dev_ap/awss_dev_ap.c
+++ src/wifi_provision/dev_ap/awss_dev_ap.c
@@ -8,12 +8,26 @@
 #if defined(__cplusplus)  /* If this is a C++ compiler, use C linkage */
 extern "C" {
 #endif
-#define AWSS_DEV_AP_WAIT_TIME_MAX_MS    (2000)
 
+#define TIMEOUT_CNT 10
 static void *g_awss_dev_ap_mutex = NULL;
 static char awss_dev_ap_switchap_done = 0;
 static char awss_dev_ap_switchap_resp_suc = 0;
 static char awss_dev_ap_ongoing = 0;
+static void start_connect_ap();
+
+typedef struct {
+    char ssid[PLATFORM_MAX_SSID_LEN + 1];
+    char passwd[PLATFORM_MAX_PASSWD_LEN + 1];
+    uint8_t bssid[ETH_ALEN];
+    uint8_t token[RANDOM_MAX_LEN + 1];
+    uint16_t msgid;
+    uint8_t cnt;
+    uint8_t token_found;
+} ap_info_t;
+ap_info_t dev_ap_info = {0};
+ap_info_t *g_dev_ap_info_ptr = &dev_ap_info;
+
 
 static int awss_dev_ap_setup()
 {
@@ -42,6 +56,22 @@ static int awss_dev_ap_setup()
         HAL_SleepMs(2000);
     }
     return ret;
+}
+
+static void try_to_do_connect_ap()
+{
+    ap_info_t *info = g_dev_ap_info_ptr;
+    int ret;
+    if (awss_dev_ap_ongoing == 0) {
+        awss_cmp_coap_cancel_packet(info->msgid);
+        return;
+    }
+
+    if (awss_dev_ap_switchap_resp_suc || ++info->cnt == TIMEOUT_CNT) {
+        start_connect_ap();
+        /* reset ssid/passwd data to 0 */
+        memset(g_dev_ap_info_ptr, 0, sizeof(ap_info_t));
+    }
 }
 
 extern int awss_success_notify(void);
@@ -87,6 +117,7 @@ int awss_dev_ap_start(void)
         if (awss_dev_ap_switchap_done) {
             break;
         }
+        try_to_do_connect_ap();
     }
     HAL_MutexUnlock(g_awss_dev_ap_mutex);
 
@@ -151,18 +182,46 @@ static int awss_dev_ap_switchap_resp(void *context, int result,
     return 0;
 }
 
+static void start_connect_ap()
+{
+    int ret = 0;
+    awss_cmp_coap_cancel_packet(g_dev_ap_info_ptr->msgid);
+    AWSS_UPDATE_STATIS(AWSS_STATIS_CONN_ROUTER_IDX, AWSS_STATIS_TYPE_TIME_START);
+    if (awss_dev_ap_ongoing == 0) {  /* interrupt by user */
+        ret = -1;
+        return;
+    }
+    HAL_Awss_Close_Ap();
+
+    ret = awss_connect(g_dev_ap_info_ptr->ssid, g_dev_ap_info_ptr->passwd, g_dev_ap_info_ptr->bssid, ETH_ALEN,
+                       g_dev_ap_info_ptr->token_found == 1 ? g_dev_ap_info_ptr->token : NULL,
+                       g_dev_ap_info_ptr->token_found == 1 ? RANDOM_MAX_LEN : 0);
+    if (ret == 0) {
+        AWSS_UPDATE_STATIS(AWSS_STATIS_CONN_ROUTER_IDX, AWSS_STATIS_TYPE_TIME_SUC);
+        awss_dev_ap_switchap_done = 1;
+        AWSS_UPDATE_STATIS(AWSS_STATIS_DAP_IDX, AWSS_STATIS_TYPE_TIME_SUC);
+    } else {
+        int ret = awss_dev_ap_setup();
+        if (0 != ret) {
+            awss_trace("retry, but awss dev ap setup fail");
+        }
+    }
+    awss_trace("connect '%s' %s\r\n", g_dev_ap_info_ptr->ssid, ret == 0 ? "success" : "fail");
+}
+
 int wifimgr_process_dev_ap_switchap_request(void *ctx, void *resource, void *remote, void *request)
 {
 #define AWSS_DEV_AP_SWITCHA_RSP_LEN (512)
-    char ssid[PLATFORM_MAX_SSID_LEN * 2 + 1] = {0}, passwd[PLATFORM_MAX_PASSWD_LEN + 1] = {0};
+    char *ssid = g_dev_ap_info_ptr->ssid;
+    char *passwd = g_dev_ap_info_ptr->passwd;
     int str_len = 0, success = 1, len = 0;
     char req_msg_id[MSG_REQ_ID_LEN] = {0};
     char random[RANDOM_MAX_LEN + 1] = {0};
-    uint8_t token[RANDOM_MAX_LEN + 1] = {0};
+    uint8_t *token = g_dev_ap_info_ptr->token;
     char *msg = NULL, *dev_info = NULL;
     char *str = NULL, *buf = NULL;
-    char bssid[ETH_ALEN] = {0};
-    char token_found = 0;
+    uint8_t *bssid = g_dev_ap_info_ptr->bssid;
+    uint8_t *token_found = &(g_dev_ap_info_ptr->token_found);
     char ssid_found = 0;
     int ret = -1;
 
@@ -238,13 +297,13 @@ int wifimgr_process_dev_ap_switchap_request(void *ctx, void *resource, void *rem
         str = json_get_value_by_name(buf, len, "token", &str_len, 0);
         if (str && str_len ==  RANDOM_MAX_LEN * 2) {  /* token len equal to random len */
             utils_str_to_hex(str, str_len, (unsigned char *)token, RANDOM_MAX_LEN);
-            token_found = 1;
+            *token_found = 1;
         }
 
         str_len = 0;
         str = json_get_value_by_name(buf, len, "bssid", &str_len, 0);
         if (str) {
-            os_wifi_str2mac(str, bssid);
+            os_wifi_str2mac(str, (char *)bssid);
         }
 
         str_len = 0;
@@ -271,52 +330,28 @@ int wifimgr_process_dev_ap_switchap_request(void *ctx, void *resource, void *rem
     awss_trace("Sending message to app: %s", msg);
     awss_trace("switch to ap: '%s'", ssid);
     if (success == 1) {
-        if (token_found == 0) {
+        if (*token_found == 0) {
             produce_random(aes_random, sizeof(aes_random));
         }
         dev_info[0] = '{';
-        awss_build_dev_info(token_found == 1 ? AWSS_NOTIFY_TYPE_MAX : AWSS_NOTIFY_DEV_BIND_TOKEN, dev_info + 1,
+        awss_build_dev_info(*token_found == 1 ? AWSS_NOTIFY_TYPE_MAX : AWSS_NOTIFY_DEV_BIND_TOKEN, dev_info + 1,
                             AWSS_DEV_AP_SWITCHA_RSP_LEN - 1);
         dev_info[strlen(dev_info)] = '}';
         dev_info[AWSS_DEV_AP_SWITCHA_RSP_LEN - 1] = '\0';
         HAL_Snprintf(msg, AWSS_DEV_AP_SWITCHA_RSP_LEN, AWSS_ACK_FMT, req_msg_id, 200, dev_info);
     }
     awss_build_topic((const char *)TOPIC_AWSS_DEV_AP_SWITCHAP, topic, TOPIC_LEN_MAX);
-    result = awss_cmp_coap_send_resp(msg, strlen(msg), remote, topic, request, awss_dev_ap_switchap_resp, &msgid, 1);
+    result = awss_cmp_coap_send_resp(msg, strlen(msg), remote, topic, request, awss_dev_ap_switchap_resp,
+                                     &(g_dev_ap_info_ptr->msgid), 1);
     (void)result;  /* remove complier warnings */
     awss_trace("sending %s.", result == 0 ? "success" : "fail");
 
     do {
-        int wait_ms = AWSS_DEV_AP_WAIT_TIME_MAX_MS;
         if (!success) {
             break;
         }
+        try_to_do_connect_ap();
 
-        while (wait_ms > 0 && awss_dev_ap_switchap_resp_suc == 0 && awss_dev_ap_ongoing) {
-            HAL_SleepMs(100);
-            wait_ms -= 100;
-        }
-        awss_cmp_coap_cancel_packet(msgid);
-        AWSS_UPDATE_STATIS(AWSS_STATIS_CONN_ROUTER_IDX, AWSS_STATIS_TYPE_TIME_START);
-        if (awss_dev_ap_ongoing == 0) {  /* interrupt by user */
-            ret = -1;
-            goto DEV_AP_SWITCHAP_END;
-        }
-        HAL_Awss_Close_Ap();
-
-        ret = awss_connect(ssid, passwd, (uint8_t *)bssid, ETH_ALEN, token_found == 1 ? token : NULL, token_found == 1 ? RANDOM_MAX_LEN : 0);
-        /*ret = HAL_Awss_Connect_Ap(WLAN_CONNECTION_TIMEOUT_MS, ssid, passwd, 0, 0, (uint8_t *)bssid, 0);*/
-        if (ret == 0) {
-            AWSS_UPDATE_STATIS(AWSS_STATIS_CONN_ROUTER_IDX, AWSS_STATIS_TYPE_TIME_SUC);
-            awss_dev_ap_switchap_done = 1;
-            AWSS_UPDATE_STATIS(AWSS_STATIS_DAP_IDX, AWSS_STATIS_TYPE_TIME_SUC);
-        } else {
-            int ret = awss_dev_ap_setup();
-            if (0 != ret) {
-                awss_trace("retry, but awss dev ap setup fail");
-            }
-        }
-        awss_trace("connect '%s' %s\r\n", ssid, ret == 0 ? "success" : "fail");
     } while (0);
 
 DEV_AP_SWITCHAP_END:
