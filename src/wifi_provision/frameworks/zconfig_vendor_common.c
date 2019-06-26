61407d854609c3b250221f13d2552872ccc00ba7
yusheng.yx <yusheng.yx@alibaba-inc.com>
2019-06-26 14:40:17 +0800

    [wifi provision] fix cannot rescan when passwd decrypt failed after channel locking

--- src/wifi_provision/frameworks/zconfig_vendor_common.c
+++ src/wifi_provision/frameworks/zconfig_vendor_common.c
@@ -341,6 +341,9 @@ rescanning:
 
         if (aws_state == AWS_SCANNING) {
             awss_debug("channel rescanning...\n");
+            if(zconfig_data != NULL) {
+                memset(zconfig_data, 0, sizeof(struct zconfig_data));
+            }
             goto rescanning;
         }
     }
