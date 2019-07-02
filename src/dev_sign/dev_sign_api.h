66e10a2c9cb15206178d8406943c38381fdc6b96
zhangtian <dongren.zt@alibaba-inc.com>
2019-07-02 14:15:48 +0800

    [dev sign]set DEV_SIGN_HOSTNAME_MAXLEN to 128 to fix out of bounds issue

--- src/dev_sign/dev_sign_api.h
+++ src/dev_sign/dev_sign_api.h
@@ -5,7 +5,7 @@
 #include "infra_defs.h"
 
 #define DEV_SIGN_SOURCE_MAXLEN    (200)
-#define DEV_SIGN_HOSTNAME_MAXLEN  (64)
+#define DEV_SIGN_HOSTNAME_MAXLEN  (128)
 #define DEV_SIGN_CLIENT_ID_MAXLEN (200)
 #define DEV_SIGN_USERNAME_MAXLEN  (64)
 #define DEV_SIGN_PASSWORD_MAXLEN  (65)
