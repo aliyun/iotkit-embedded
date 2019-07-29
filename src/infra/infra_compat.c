093a48b22b27db03561009b9040e6bdb65e1b126
yusheng.yx <yusheng.yx@alibaba-inc.com>
2019-07-29 17:40:55 +0800

    [infra] fix info lost caused by clean-up of IoT_Ioctl()

--- src/infra/infra_compat.c
+++ src/infra/infra_compat.c
@@ -93,13 +93,7 @@ int IOT_Ioctl(int option, void *data)
         case IOTX_IOCTL_SET_MQTT_DOMAIN: {
             ctx->domain_type = IOTX_CLOUD_REGION_CUSTOM;
 
-            if (ctx->cloud_custom_domain) {
-                HAL_Free(ctx->cloud_custom_domain);
-                ctx->cloud_custom_domain = NULL;
-                g_infra_mqtt_domain[IOTX_CLOUD_REGION_CUSTOM] = NULL;
-            }
-            ctx->cloud_custom_domain = HAL_Malloc(strlen((char *)data) + 1);
-            if (ctx->cloud_custom_domain == NULL) {
+            if (strlen(data) > IOTX_DOMAIN_MAX_LEN) {
                 return FAIL_RETURN;
             }
             memset(ctx->cloud_custom_domain, 0, strlen((char *)data) + 1);
@@ -111,13 +105,7 @@ int IOT_Ioctl(int option, void *data)
         case IOTX_IOCTL_SET_HTTP_DOMAIN: {
             ctx->domain_type = IOTX_HTTP_REGION_CUSTOM;
 
-            if (ctx->http_custom_domain) {
-                HAL_Free(ctx->http_custom_domain);
-                ctx->http_custom_domain = NULL;
-                g_infra_http_domain[IOTX_CLOUD_REGION_CUSTOM] = NULL;
-            }
-            ctx->http_custom_domain = HAL_Malloc(strlen((char *)data) + 1);
-            if (ctx->http_custom_domain == NULL) {
+            if (strlen(data) > IOTX_DOMAIN_MAX_LEN) {
                 return FAIL_RETURN;
             }
             memset(ctx->http_custom_domain, 0, strlen((char *)data) + 1);
@@ -185,12 +173,7 @@ int IOT_Ioctl(int option, void *data)
         break;
 #endif
         case IOTX_IOCTL_SET_CUSTOMIZE_INFO: {
-            if (ctx->mqtt_customzie_info) {
-                HAL_Free(ctx->mqtt_customzie_info);
-                ctx->mqtt_customzie_info = NULL;
-            }
-            ctx->mqtt_customzie_info = HAL_Malloc(strlen((char *)data) + 1);
-            if (ctx->mqtt_customzie_info == NULL) {
+            if (strlen(data) > IOTX_CUSTOMIZE_INFO_LEN) {
                 return FAIL_RETURN;
             }
             memset(ctx->mqtt_customzie_info, 0, strlen((char *)data) + 1);
