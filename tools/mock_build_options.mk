ed34bc9cf071847f6fb90b7248ad680455914e78
yusheng <yusheng.yx@alibaba-inc.com>
2019-07-30 13:21:01 +0800

    [tools] remove wifi-provision and dev-bind from MacOS build
    Signed-off-by: yusheng <yusheng.yx@alibaba-inc.com>

--- tools/mock_build_options.mk
+++ tools/mock_build_options.mk
@@ -2,3 +2,9 @@
 ifeq (y,$(strip $(FEATURE_HTTP2_COMM_ENABLED)))
     CFLAGS := $(filter-out -DFORCE_SSL_VERIFY,$(CFLAGS))
 endif
+
+ifeq (Darwin,$(shell uname))
+    CFLAGS := $(filter-out -DOTA_ENABLED,$(CFLAGS))
+    CFLAGS := $(filter-out -DWIFI_PROVISION_ENABLED,$(CFLAGS))
+    CFLAGS := $(filter-out -DDEV_BIND_ENABLED,$(CFLAGS))
+endif
