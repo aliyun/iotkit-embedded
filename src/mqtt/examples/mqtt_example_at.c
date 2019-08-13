792da971300a15297651749a99114a9364463c51
jianzhou <jianzhou.cjz@alibaba-inc.com>
2019-08-13 06:08:34 -0700

    [atm] update mqtt at example

--- src/mqtt/examples/mqtt_example_at.c
+++ src/mqtt/examples/mqtt_example_at.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include <string.h>
-
+#include "infra_compat.h"
 #include "dev_sign_api.h"
 #include "mqtt_api.h"
 #include "wrappers.h"
-#include "infra_compat.h"
 #ifdef ATM_ENABLED
     #include "at_api.h"
 #endif
@@ -13,8 +11,6 @@ char g_product_key[IOTX_PRODUCT_KEY_LEN + 1]       = "a1RIsMLz2BJ";
 char g_product_secret[IOTX_PRODUCT_SECRET_LEN + 1] = "fSAF0hle6xL0oRWd";
 char g_device_name[IOTX_DEVICE_NAME_LEN + 1]       = "example1";
 char g_device_secret[IOTX_DEVICE_SECRET_LEN + 1]   = "RDXf67itLqZCwdMCRrw0N5FHbv5D7jrE";
-
-static char g_topic_name[CONFIG_MQTT_TOPIC_MAXLEN];
 
 #define EXAMPLE_TRACE(fmt, ...)  \
     do { \
@@ -30,10 +26,10 @@ void example_message_arrive(void *pcontext, void *pclient, iotx_mqtt_event_msg_p
     switch (msg->event_type) {
         case IOTX_MQTT_EVENT_PUBLISH_RECEIVED:
             /* print topic name and topic message */
-            HAL_Printf("Message Arrived: \n");
-            HAL_Printf("Topic  : %.*s\n", topic_info->topic_len, topic_info->ptopic);
-            HAL_Printf("Payload: %.*s\n", topic_info->payload_len, topic_info->payload);
-            HAL_Printf("\n");
+            EXAMPLE_TRACE("Message Arrived:");
+            EXAMPLE_TRACE("Topic  : %.*s", topic_info->topic_len, topic_info->ptopic);
+            EXAMPLE_TRACE("Payload: %.*s", topic_info->payload_len, topic_info->payload);
+            EXAMPLE_TRACE("\n");
             break;
         default:
             break;
@@ -48,61 +44,62 @@ int example_subscribe(void *handle)
     int topic_len = 0;
 
     topic_len = strlen(fmt) + strlen(g_product_key) + strlen(g_device_name) + 1;
-    if (topic_len > CONFIG_MQTT_TOPIC_MAXLEN) {
-        HAL_Printf("topic too long\n");
+    topic = HAL_Malloc(topic_len);
+    if (topic == NULL) {
+        EXAMPLE_TRACE("memory not enough");
         return -1;
     }
-    topic = g_topic_name;
-    memset(topic, 0, CONFIG_MQTT_TOPIC_MAXLEN);
+    memset(topic, 0, topic_len);
     HAL_Snprintf(topic, topic_len, fmt, g_product_key, g_device_name);
 
     res = IOT_MQTT_Subscribe(handle, topic, IOTX_MQTT_QOS0, example_message_arrive, NULL);
     if (res < 0) {
-        HAL_Printf("subscribe failed\n");
+        EXAMPLE_TRACE("subscribe failed");
+        HAL_Free(topic);
         return -1;
     }
 
+    HAL_Free(topic);
     return 0;
 }
 
 int example_publish(void *handle)
 {
-    int res = 0;
-    iotx_mqtt_topic_info_t topic_msg;
-    const char *fmt = "/%s/%s/user/get";
-    char *topic = NULL;
-    int topic_len = 0;
-    char *payload = "hello,world";
+    int             res = 0;
+    const char     *fmt = "/%s/%s/user/get";
+    char           *topic = NULL;
+    int             topic_len = 0;
+    char           *payload = "{\"message\":\"hello!\"}";
 
     topic_len = strlen(fmt) + strlen(g_product_key) + strlen(g_device_name) + 1;
-    if (topic_len > CONFIG_MQTT_TOPIC_MAXLEN) {
-        HAL_Printf("topic too long\n");
+    topic = HAL_Malloc(topic_len);
+    if (topic == NULL) {
+        EXAMPLE_TRACE("memory not enough");
         return -1;
     }
-    topic = g_topic_name;
-    memset(topic, 0, CONFIG_MQTT_TOPIC_MAXLEN);
+    memset(topic, 0, topic_len);
     HAL_Snprintf(topic, topic_len, fmt, g_product_key, g_device_name);
 
-
-    memset(&topic_msg, 0x0, sizeof(iotx_mqtt_topic_info_t));
-    topic_msg.qos = IOTX_MQTT_QOS0;
-    topic_msg.retain = 0;
-    topic_msg.dup = 0;
-    topic_msg.payload = (void *)payload;
-    topic_msg.payload_len = strlen(payload);
-
-    res = IOT_MQTT_Publish(handle, topic, &topic_msg);
+    res = IOT_MQTT_Publish_Simple(0, topic, IOTX_MQTT_QOS0, payload, strlen(payload));
     if (res < 0) {
-        HAL_Printf("publish failed\n");
+        EXAMPLE_TRACE("publish failed, res = %d", res);
+        HAL_Free(topic);
         return -1;
     }
 
+    HAL_Free(topic);
     return 0;
 }
 
 void example_event_handle(void *pcontext, void *pclient, iotx_mqtt_event_msg_pt msg)
 {
-    HAL_Printf("msg->event_type : %d\n", msg->event_type);
+    EXAMPLE_TRACE("msg->event_type : %d", msg->event_type);
+}
+
+int everything_state_handle(const int state_code, const char * state_message)
+{
+    EXAMPLE_TRACE("code: -0x%04x, message: %s", -state_code, state_message);
+    return 0;
 }
 
 /*
@@ -111,23 +108,19 @@ void example_event_handle(void *pcontext, void *pclient, iotx_mqtt_event_msg_pt 
  *  The demo device has been configured in IoT console (https://iot.console.aliyun.com)
  *  so that its /${productKey}/${deviceName}/user/get can both be subscribed and published
  *
- *  We design this to completely demostrate publish & subscribe process, in this way
+ *  We design this to completely demonstrate publish & subscribe process, in this way
  *  MQTT client can receive original packet sent by itself
  *
- *  For new devices created by yourself, pub/sub privilege also required to be granted
- *  to its /${productKey}/${deviceName}/user/get to run whole example
+ *  For new devices created by yourself, pub/sub privilege also requires being granted
+ *  to its /${productKey}/${deviceName}/user/get for successfully running whole example
  */
 
 int main(int argc, char *argv[])
 {
-    void       *pclient = NULL;
-    int         res = 0;
-    int         loop_cnt = 0;
-
-    iotx_mqtt_region_types_t    region = IOTX_CLOUD_REGION_SHANGHAI;
-    iotx_sign_mqtt_t            sign_mqtt;
-    iotx_dev_meta_info_t        meta;
-    iotx_mqtt_param_t           mqtt_params;
+    void                   *pclient = NULL;
+    int                     res = 0;
+    int                     loop_cnt = 0;
+    iotx_mqtt_param_t       mqtt_params;
 
 #ifdef ATM_ENABLED
     if (IOT_ATM_Init() < 0) {
@@ -135,47 +128,113 @@ int main(int argc, char *argv[])
         return -1;
     }
 #endif
-    HAL_Printf("mqtt example\n");
 
-    IOT_Ioctl(IOTX_IOCTL_SET_PRODUCT_KEY, g_product_key); 
+    IOT_Ioctl(IOTX_IOCTL_SET_PRODUCT_KEY, g_product_key);
     IOT_Ioctl(IOTX_IOCTL_SET_DEVICE_NAME, g_device_name);
+    IOT_Ioctl(IOTX_IOCTL_SET_DEVICE_SECRET, g_device_secret);
 
-    memset(&meta, 0, sizeof(iotx_dev_meta_info_t));
-    memcpy(meta.product_key, g_product_key, strlen(g_product_key));
-    memcpy(meta.device_name, g_device_name, strlen(g_device_name));
-    memcpy(meta.device_secret, g_device_secret, strlen(g_device_secret));
+    IOT_RegisterCallback(ITE_STATE_EVERYTHING, everything_state_handle);
 
-    memset(&sign_mqtt, 0x0, sizeof(iotx_sign_mqtt_t));
-
-    if (IOT_Sign_MQTT(region, &meta, &sign_mqtt) < 0) {
-        return -1;
-    }
-
-#if 0   /* Uncomment this to show more information */
-    HAL_Printf("sign_mqtt.hostname: %s\n", sign_mqtt.hostname);
-    HAL_Printf("sign_mqtt.port    : %d\n", sign_mqtt.port);
-    HAL_Printf("sign_mqtt.username: %s\n", sign_mqtt.username);
-    HAL_Printf("sign_mqtt.password: %s\n", sign_mqtt.password);
-    HAL_Printf("sign_mqtt.clientid: %s\n", sign_mqtt.clientid);
-#endif
+    EXAMPLE_TRACE("mqtt example");
 
     /* Initialize MQTT parameter */
+    /*
+     * Note:
+     *
+     * If you did NOT set value for members of mqtt_params, SDK will use their default values
+     * If you wish to customize some parameter, just un-comment value assigning expressions below
+     *
+     **/
     memset(&mqtt_params, 0x0, sizeof(mqtt_params));
 
-    mqtt_params.port = sign_mqtt.port;
-    mqtt_params.host = sign_mqtt.hostname;
-    mqtt_params.client_id = sign_mqtt.clientid;
-    mqtt_params.username = sign_mqtt.username;
-    mqtt_params.password = sign_mqtt.password;
+    /**
+     *
+     *  MQTT connect hostname string
+     *
+     *  MQTT server's hostname can be customized here
+     *
+     *  default value is ${productKey}.iot-as-mqtt.cn-shanghai.aliyuncs.com
+     */
+    /* mqtt_params.host = "something.iot-as-mqtt.cn-shanghai.aliyuncs.com"; */
 
-    mqtt_params.request_timeout_ms = 2000;
-    mqtt_params.clean_session = 0;
-    mqtt_params.keepalive_interval_ms = 60000;
-    mqtt_params.read_buf_size = 1024;
-    mqtt_params.write_buf_size = 1024;
+    /**
+     *
+     *  MQTT connect port number
+     *
+     *  TCP/TLS port which can be 443 or 1883 or 80 or etc, you can customize it here
+     *
+     *  default value is 1883 in TCP case, and 443 in TLS case
+     */
+    /* mqtt_params.port = 1883; */
 
+    /**
+     *
+     * MQTT request timeout interval
+     *
+     * MQTT message request timeout for waiting ACK in MQTT Protocol
+     *
+     * default value is 2000ms.
+     */
+    /* mqtt_params.request_timeout_ms = 2000; */
+
+    /**
+     *
+     * MQTT clean session flag
+     *
+     * If CleanSession is set to 0, the Server MUST resume communications with the Client based on state from
+     * the current Session (as identified by the Client identifier).
+     *
+     * If CleanSession is set to 1, the Client and Server MUST discard any previous Session and Start a new one.
+     *
+     * default value is 0.
+     */
+    /* mqtt_params.clean_session = 0; */
+
+    /**
+     *
+     * MQTT keepAlive interval
+     *
+     * KeepAlive is the maximum time interval that is permitted to elapse between the point at which
+     * the Client finishes transmitting one Control Packet and the point it starts sending the next.
+     *
+     * default value is 60000.
+     */
+    /* mqtt_params.keepalive_interval_ms = 60000; */
+
+    /**
+     *
+     * MQTT write buffer size
+     *
+     * Write buffer is allocated to place upstream MQTT messages, MQTT client will be limitted
+     * to send packet no longer than this to Cloud
+     *
+     * default value is 1024.
+     *
+     */
+    /* mqtt_params.write_buf_size = 1024; */
+
+    /**
+     *
+     * MQTT read buffer size
+     *
+     * Write buffer is allocated to place downstream MQTT messages, MQTT client will be limitted
+     * to recv packet no longer than this from Cloud
+     *
+     * default value is 1024.
+     *
+     */
+    /* mqtt_params.read_buf_size = 1024; */
+
+    /**
+     *
+     * MQTT event callback function
+     *
+     * Event callback function will be called by SDK when it want to notify user what is happening inside itself
+     *
+     * default value is NULL, which means PUB/SUB event won't be exposed.
+     *
+     */
     mqtt_params.handle_event.h_fp = example_event_handle;
-    mqtt_params.handle_event.pcontext = NULL;
 
     pclient = IOT_MQTT_Construct(&mqtt_params);
     if (NULL == pclient) {
