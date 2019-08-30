7c910714e98f05f3097043a9197154e1e8edab7b
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2019-08-30 20:56:34 +0800

    add mqtt_example_auto.c
    update mqtt_example_auto target name

--- src/mqtt/iot.mk
+++ src/mqtt/iot.mk
@@ -3,11 +3,13 @@ HDR_REFS            := src/infra
 LIB_SRCS_PATTERN    := *.c
 
 SRCS_mqtt-example       := examples/mqtt_example.c $(TOP_DIR)/extension/state_desc.c
+SRCS_mqtt-example-auto  := examples/mqtt_example_auto.c examples/cJSON.c
 SRCS_mqtt-task-example  := examples/mqtt_task_example.c examples/cJSON.c
 SRCS_mqtt-example-at    := examples/mqtt_example_at.c
 
 $(call Append_Conditional, LIB_SRCS_PATTERN, impl/*.c, MQTT_DEFAULT_IMPL)
 $(call Append_Conditional, TARGET, mqtt-example, MQTT_COMM_ENABLED, ATM_ENABLED BUILD_AOS NO_EXECUTABLES)
+$(call Append_Conditional, TARGET, mqtt-example-auto, MQTT_COMM_ENABLED, ATM_ENABLED BUILD_AOS NO_EXECUTABLES)
 $(call Append_Conditional, TARGET, mqtt-task-example, MQTT_COMM_ENABLED, ATM_ENABLED BUILD_AOS NO_EXECUTABLES)
 $(call Append_Conditional, TARGET, mqtt-example-at, ATM_ENABLED, BUILD_AOS NO_EXECUTABLES)
 
