ecc2d509f93894b38467de156fd1f1601ad91d60
yinghao.cyh <yinghao.cyh@alibaba-inc.com>
2020-04-17 14:46:54 +0800

    [dev-model] fix subdev topic unsub issue
    Signed-off-by: yinghao.cyh <yinghao.cyh@alibaba-inc.com>

--- src/infra/infra_defs.h
+++ src/infra/infra_defs.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-#define IOTX_SDK_REVISION               "20200312.tools-configuration-INFRA_LOG-should-be-mandantory"
+#define IOTX_SDK_REVISION               "20200417.dev-model-fix-subdev-topic-unsub-issue"
 #define IOTX_SDK_VERSION                "3.1.0"
 #define IOTX_ALINK_VERSION              "20"
 #define IOTX_PRODUCT_KEY_LEN            (20)
