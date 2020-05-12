bc1ed3da865f94687360c1bca652b0ac71fffcfd
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2020-05-12 15:17:26 +0800

    [infra] restore http domain

--- src/infra/infra_defs.c
+++ src/infra/infra_defs.c
@@ -15,9 +15,9 @@ const char *g_infra_mqtt_domain[IOTX_MQTT_DOMAIN_NUMBER] = {
 const char *g_infra_http_domain[IOTX_HTTP_DOMAIN_NUMBER] = {
     "iot-auth.cn-shanghai.aliyuncs.com",         /* Shanghai */
     "iot-auth.ap-southeast-1.aliyuncs.com",      /* Singapore */
-    "iot-auth.ap-southeast-1.aliyuncs.com",      /* Japan */
-    "iot-auth.ap-southeast-1.aliyuncs.com",      /* America */
-    "iot-auth.ap-southeast-1.aliyuncs.com",      /* Germany */
+    "iot-auth.ap-northeast-1.aliyuncs.com",      /* Japan */
+    "iot-auth.us-west-1.aliyuncs.com",           /* America */
+    "iot-auth.eu-central-1.aliyuncs.com",        /* Germany */
     NULL,                                        /* Custom */
 };
 
--- src/infra/infra_defs.h
+++ src/infra/infra_defs.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-#define IOTX_SDK_REVISION               "20200423.http2-fix-storeid-missing-issue"
+#define IOTX_SDK_REVISION               "20200512.infra-restore-http-domain"
 #define IOTX_SDK_VERSION                "3.2.0"
 #define IOTX_ALINK_VERSION              "20"
 #define IOTX_PRODUCT_KEY_LEN            (20)
