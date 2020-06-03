cfe4a1b99611df5de526072776b58c837f884a6e
xicai.cxc <xicai.cxc@alibaba-inc.com>
2020-06-03 14:15:22 +0800

    [ota] fix wrong device secret issue in ota demo

--- src/infra/infra_defs.h
+++ src/infra/infra_defs.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-#define IOTX_SDK_REVISION               "20200512.infra-restore-http-domain"
+#define IOTX_SDK_REVISION               "20200603.ota-fix-wrong-device-secret-issue-in-ota-demo"
 #define IOTX_SDK_VERSION                "3.2.0"
 #define IOTX_ALINK_VERSION              "20"
 #define IOTX_PRODUCT_KEY_LEN            (20)
